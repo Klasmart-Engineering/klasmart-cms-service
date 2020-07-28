0b5d9a00fadc8300cfcaaf42964c1e20f48783c2
White <white.shan@badanamu.com.cn>
2020-07-28 09:01:53 +0800

    update

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package kidsloop2
 
 import(
+	"calmisland/kidsloop2/dynamodb"
 	"context"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,13 +12,16 @@ func response(statusCode int, body string) events.APIGatewayProxyResponse{
 	return events.APIGatewayProxyResponse{
 		StatusCode:        statusCode,
 		Body:              body,
-	}
-}
+	}}
 
 func doLambda(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return response(200, "success"), nil
+
 }
 
 func main() {
+	//获取数据库连接
+	dynamodb.GetClient()
+
 	lambda.Start(doLambda)
 }
