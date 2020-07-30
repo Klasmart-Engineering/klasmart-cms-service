db9b7ea31e087ec976e23876afe8dd54bc51d6e7
brilliant <brillant.yang@badanamu.com.cn>
2020-07-30 10:51:35 +0800

    merge dev

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
-package kidsloop2
+package main
 
 import (
-	"calmisland/kidsloop2/api"
-	"calmisland/kidsloop2/dynamodb"
-	"calmisland/kidsloop2/storage"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"gitlab.badanamu.com.cn/calmisland/common-cn/common"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/api"
+	_ "gitlab.badanamu.com.cn/calmisland/kidsloop2/config"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/dynamodb"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/storage"
+	"go.uber.org/zap"
 )
 
 func main() {
+	// init logger
+	lc := zap.NewDevelopmentConfig()
+	lc.Encoding = "json"
+
+	logger, _ := lc.Build()
+	defer logger.Sync()
+
+	zap.ReplaceGlobals(logger)
+
 	// init dynamodb connection
 	dynamodb.GetClient()
 	storage.DefaultStorage()
 
+	zap.L().Info("start kidsloop2 api service")
+	defer zap.L().Info("kidsloop2 api service stopped")
+
 	common.Setenv(common.EnvLAMBDA)
-	common.RunWithHTTPHandler(api.NewServer(), "")
+	go common.RunWithHTTPHandler(api.NewServer(), "")
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
 }
