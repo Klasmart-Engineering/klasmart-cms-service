f7fd49d6248d9a122846c6763dd0de2a32917584
Medivh <ext.zhu.yang@badanamu.com.cn>
2020-08-18 15:04:12 +0800

    feat: add schedule repeat options entity define

---
+++ entity/schedule.go
@@ -0,0 +1,154 @@
+package entity
+
+type RepeatType string
+
+const (
+	RepeatTypeDaily   RepeatType = "daily"
+	RepeatTypeWeekly  RepeatType = "weekly"
+	RepeatTypeMonthly RepeatType = "monthly"
+	RepeatTypeYearly  RepeatType = "yearly"
+)
+
+func (t RepeatType) Valid() bool {
+	switch t {
+	case RepeatTypeDaily, RepeatTypeWeekly, RepeatTypeMonthly, RepeatTypeYearly:
+		return true
+	default:
+		return false
+	}
+}
+
+type RepeatEndType string
+
+const (
+	RepeatEndNever      RepeatEndType = "never"
+	RepeatEndAfterCount RepeatEndType = "after_count"
+	RepeatEndAfterTime  RepeatEndType = "after_time"
+)
+
+type RepeatWeek string
+
+const (
+	RepeatWeekSun RepeatWeek = "sun"
+	RepeatWeekMon RepeatWeek = "mon"
+	RepeatWeekTue RepeatWeek = "tue"
+	RepeatWeekWed RepeatWeek = "wed"
+	RepeatWeekThu RepeatWeek = "thu"
+	RepeatWeekFri RepeatWeek = "fri"
+	RepeatWeekSat RepeatWeek = "sat"
+)
+
+func (w RepeatWeek) Valid() bool {
+	switch w {
+	case RepeatWeekSun, RepeatWeekMon, RepeatWeekTue, RepeatWeekWed, RepeatWeekThu, RepeatWeekFri, RepeatWeekSat:
+		return true
+	default:
+		return false
+	}
+}
+
+type RepeatMonthlyOnType string
+
+const (
+	RepeatMonthlyOnDate RepeatMonthlyOnType = "date"
+	RepeatMonthlyOnWeek RepeatMonthlyOnType = "week"
+)
+
+func (t RepeatMonthlyOnType) Valid() bool {
+	switch t {
+	case RepeatMonthlyOnDate, RepeatMonthlyOnWeek:
+		return true
+	default:
+		return false
+	}
+}
+
+type RepeatYearlyOnType string
+
+const (
+	RepeatYearlyOnDate RepeatYearlyOnType = "date"
+	RepeatYearlyOnWeek RepeatYearlyOnType = "week"
+)
+
+func (t RepeatYearlyOnType) Valid() bool {
+	switch t {
+	case RepeatYearlyOnDate, RepeatYearlyOnWeek:
+		return true
+	default:
+		return false
+	}
+}
+
+func (t RepeatEndType) Valid() bool {
+	switch t {
+	case RepeatEndNever, RepeatEndAfterCount, RepeatEndAfterTime:
+		return true
+	default:
+		return false
+	}
+}
+
+type RepeatWeekSeq string
+
+const (
+	RepeatWeekSeqFirst  RepeatWeekSeq = "first"
+	RepeatWeekSeqSecond RepeatWeekSeq = "second"
+	RepeatWeekSeqThird  RepeatWeekSeq = "third"
+	RepeatWeekSeqFourth RepeatWeekSeq = "fourth"
+	RepeatWeekSeqLast   RepeatWeekSeq = "last"
+)
+
+func (s RepeatWeekSeq) Valid() bool {
+	switch s {
+	case RepeatWeekSeqFirst, RepeatWeekSeqSecond, RepeatWeekSeqThird, RepeatWeekSeqFourth, RepeatWeekSeqLast:
+		return true
+	default:
+		return false
+	}
+}
+
+type RepeatOptions struct {
+	Type    RepeatType    `json:"type"`
+	Daily   RepeatDaily   `json:"daily"`
+	Weekly  RepeatWeekly  `json:"weekly"`
+	Monthly RepeatMonthly `json:"monthly"`
+	Yearly  RepeatYearly  `json:"yearly"`
+}
+
+type RepeatDaily struct {
+	Interval int       `json:"interval"`
+	End      RepeatEnd `json:"end"`
+}
+
+type RepeatWeekly struct {
+	Interval int        `json:"interval"`
+	On       RepeatWeek `json:"on"`
+	End      RepeatEnd  `json:"end"`
+}
+
+type RepeatMonthly struct {
+	Interval  int                 `json:"interval"`
+	OnType    RepeatMonthlyOnType `json:"on_type"`
+	OnDateDay int                 `json:"on_date_day"`
+	OnWeekSeq RepeatWeekSeq       `json:"on_week_seq"`
+	OnWeek    RepeatWeek          `json:"on_week"`
+	End       RepeatEndType       `json:"end"`
+}
+
+type RepeatYearly struct {
+	Interval    int                `json:"interval"`
+	OnType      RepeatYearlyOnType `json:"on_type"`
+	OnDateMonth int                `json:"on_date_month"`
+	OnDateDay   int                `json:"on_date_day"`
+	OnWeekMonth int                `json:"on_week_month"`
+	OnWeekSeq   RepeatWeekSeq      `json:"on_week_seq"`
+	OnWeek      RepeatWeek         `json:"on_week"`
+	End         RepeatEndType      `json:"end"`
+}
+
+type RepeatEnd struct {
+	Type       RepeatEndType `json:"type"`
+	Never      bool          `json:"never"`
+	AfterCount int           `json:"after_count"`
+	AfterTime  int64         `json:"after_time"`
+}
