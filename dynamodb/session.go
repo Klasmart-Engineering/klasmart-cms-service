46443b68a614b5564772cccb5987b637fce82271
Medivh <ext.zhu.yang@badanamu.com.cn>
2020-08-19 17:00:12 +0800

    feat: add lock log test

--- dynamodb/session.go
+++ dynamodb/session.go
@@ -17,8 +17,8 @@ func GetClient() *dynamodb.DynamoDB {
 	_once.Do(func() {
 		sess := session.Must(session.NewSessionWithOptions(session.Options{
 			Config: aws.Config{
-				Region: aws.String("ap-northeast-2"),
-				//Endpoint: aws.String("http://192.168.1.234:18000"),
+				Region:   aws.String("ap-northeast-2"),
+				Endpoint: aws.String("http://192.168.1.234:18000"),
 			},
 			SharedConfigState: session.SharedConfigEnable,
 		}))
