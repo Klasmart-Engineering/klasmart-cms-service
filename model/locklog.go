90d5277297ec63d3781648614f6cb074319c5409
Medivh <ext.zhu.yang@badanamu.com.cn>
2020-08-19 17:13:48 +0800

    refactor: rename

--- model/locklog.go
+++ model/locklog.go
@@ -7,7 +7,7 @@ import (
 
 type ILockLogModel interface {
 	IsLock(ctx context.Context, recordID string) (bool, error)
-	Lock(ctx context.Context, recordID string, userID string) error
+	Lock(ctx context.Context, recordID string, operatorID string) error
 	Unlock(ctx context.Context, recordID string) error
 }
 
@@ -29,7 +29,7 @@ func (l *lockLogModel) IsLock(ctx context.Context, recordID string) (bool, error
 	panic("implement me")
 }
 
-func (l *lockLogModel) Lock(ctx context.Context, recordID string, userID string) error {
+func (l *lockLogModel) Lock(ctx context.Context, recordID string, operatorID string) error {
 	panic("implement me")
 }
 
