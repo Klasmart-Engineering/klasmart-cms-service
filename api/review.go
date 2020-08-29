994d98aa21fccf31cc12624cb28593b316289f5f
Brilliant <carl_yh@126.com>
2020-08-29 20:26:16 +0800

    log

--- api/review.go
+++ api/review.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"gitlab.badanamu.com.cn/calmisland/common-log/log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,10 +19,12 @@ func (s *Server) approve(c *gin.Context) {
 	err := model.GetReviewerModel().Approve(ctx, dbo.MustGetDB(ctx), cid, op)
 	switch err {
 	case model.ErrNoContent:
+		log.Error(ctx, "approve", log.Any("op", op), log.String("cid", cid), log.Err(err))
 		c.JSON(http.StatusNotFound, "content not found")
 	case nil:
 		c.JSON(http.StatusOK, "ok")
 	default:
+		log.Error(ctx, "approve", log.Any("op", op), log.String("cid", cid), log.Err(err))
 		// TODO: differentiate error types
 		c.JSON(http.StatusInternalServerError, "Internal server error")
 	}
@@ -38,11 +41,12 @@ func (s *Server) reject(c *gin.Context) {
 	err := model.GetReviewerModel().Reject(ctx, dbo.MustGetDB(ctx), cid, "", op)
 	switch err {
 	case model.ErrNoContent:
+		log.Error(ctx, "reject", log.Any("op", op), log.String("cid", cid), log.Err(err))
 		c.JSON(http.StatusNotFound, "content not found")
 	case nil:
 		c.JSON(http.StatusOK, "ok")
-
 	default:
+		log.Error(ctx, "reject", log.Any("op", op), log.String("cid", cid), log.Err(err))
 		c.JSON(http.StatusInternalServerError, "Internal server error")
 	}
 }
