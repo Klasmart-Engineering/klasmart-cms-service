a1d99884305d85a57735c83f944fd474c29c4398
shanyl2400 <white.shan@badanamu.com.cn>
2020-08-31 14:54:48 +0800

    fix bugs in prepare result

--- api/route.go
+++ api/route.go
@@ -15,7 +15,6 @@ func (s Server) registeRoute() {
 	s.engine.NoRoute(func(c *gin.Context) {
 		c.AbortWithError(http.StatusNotFound, errRouteNotFound)
 	})
-
 	s.engine.GET("/v1/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
