6ad59c25f1fa40cca0f3756918e1f5dc6f39e4d2
shanyl2400 <white.shan@badanamu.com.cn>
2020-09-02 13:34:51 +0800

    update test

--- model/storage/s3storage_test.go
+++ model/storage/s3storage_test.go
@@ -9,10 +9,10 @@ import (
 
 func InitEnv() {
 	os.Setenv("cloud_env", "aws")
-	os.Setenv("storage_bucket", "kidsloop-global-resources-dev")
+	os.Setenv("storage_bucket", "kidsloop-global-resources-test")
 	os.Setenv("storage_region", "ap-northeast-2")
-	os.Setenv("secret_id", "AKIAXGKUAYT2P2IJ2KX7")
-	os.Setenv("secret_key", "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")
+	os.Setenv("AWS_ACCESS_KEY_ID", "AKIAXGKUAYT2P2IJ2KX7")
+	os.Setenv("AWS_SECRET_ACCESS_KEY", "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")
 	os.Setenv("storage_accelerate", "true")
 	os.Setenv("db_env", "mysql")
 	os.Setenv("cdn_open", "true")
