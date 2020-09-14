de3b4faa486cbfb84ca9f77aa34a288aebf366ae
kwok.guo <kwok.guo@badanamu.com.cn>
2020-09-14 13:42:41 +0800

    [feature]test live token

--- utils/jwt_test.go
+++ utils/jwt_test.go
@@ -1,28 +1,24 @@
 package utils
 
 import (
-	"context"
-	"github.com/dgrijalva/jwt-go"
-	"gitlab.badanamu.com.cn/calmisland/kidsloop2/entity"
 	"testing"
-	"time"
 )
 
 func TestCreateJWT(t *testing.T) {
-	now := time.Now()
-	stdClaims := &jwt.StandardClaims{
-		Audience:  "kidsloop-live",
-		ExpiresAt: now.Add(time.Hour * 24 * entity.ValidDays).Unix(),
-		IssuedAt:  now.Add(-30 * time.Second).Unix(),
-		Issuer:    "KidsLoopUser-live",
-		NotBefore: 0,
-		Subject:   "authorization",
-	}
-
-	token, err := CreateJWT(context.Background(), stdClaims)
-	if err != nil {
-		t.Log(err)
-		return
-	}
-	t.Log(token)
+	//now := time.Now()
+	//stdClaims := &jwt.StandardClaims{
+	//	Audience:  "kidsloop-live",
+	//	ExpiresAt: now.Add(time.Hour * 24 * entity.ValidDays).Unix(),
+	//	IssuedAt:  now.Add(-30 * time.Second).Unix(),
+	//	Issuer:    "KidsLoopUser-live",
+	//	NotBefore: 0,
+	//	Subject:   "authorization",
+	//}
+	//
+	//token, err := CreateJWT(context.Background(), stdClaims)
+	//if err != nil {
+	//	t.Log(err)
+	//	return
+	//}
+	//t.Log(token)
 }
