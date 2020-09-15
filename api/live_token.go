810965734122006711cae3532e00013c04220fd8
peter.yang <peter.yang@badanamu.com.cn>
2020-09-15 15:50:26 +0800

    [feature]response 404 if content is not exists

--- api/live_token.go
+++ api/live_token.go
@@ -1,10 +1,12 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"gitlab.badanamu.com.cn/calmisland/common-log/log"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/constant"
 	"gitlab.badanamu.com.cn/calmisland/kidsloop2/model"
-	"net/http"
 )
 
 func (s *Server) getScheduleLiveToken(c *gin.Context) {
@@ -12,14 +14,18 @@ func (s *Server) getScheduleLiveToken(c *gin.Context) {
 	ctx := c.Request.Context()
 	scheduleID := c.Param("id")
 	token, err := model.GetLiveTokenModel().MakeLiveToken(ctx, op, scheduleID)
-	if err != nil {
-		log.Error(ctx, "make schedule live token error", log.String("scheduleID", scheduleID), log.Err(err))
+	switch err {
+	case nil:
+		c.JSON(http.StatusOK, gin.H{
+			"token": token,
+		})
+	case constant.ErrRecordNotFound:
+		log.Info(ctx, "schedule not found", log.Err(err), log.String("scheduleID", scheduleID))
+		c.JSON(http.StatusNotFound, L(Unknown))
+	default:
+		log.Error(ctx, "make schedule live token error", log.Err(err), log.String("scheduleID", scheduleID))
 		c.JSON(http.StatusInternalServerError, L(Unknown))
-		return
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"token": token,
-	})
 }
 
 func (s *Server) getContentLiveToken(c *gin.Context) {
@@ -27,12 +33,17 @@ func (s *Server) getContentLiveToken(c *gin.Context) {
 	ctx := c.Request.Context()
 	contentID := c.Param("content_id")
 	token, err := model.GetLiveTokenModel().MakeLivePreviewToken(ctx, op, contentID)
-	if err != nil {
-		log.Error(ctx, "make content live token error", log.String("contentID", contentID), log.Err(err))
+	switch err {
+	case nil:
+		c.JSON(http.StatusOK, gin.H{
+			"token": token,
+		})
+	case constant.ErrRecordNotFound:
+		log.Info(ctx, "content not found", log.Err(err), log.String("contentID", contentID))
+		c.JSON(http.StatusNotFound, L(Unknown))
+
+	default:
+		log.Error(ctx, "make content live token error", log.Err(err), log.String("contentID", contentID))
 		c.JSON(http.StatusInternalServerError, L(Unknown))
-		return
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"token": token,
-	})
 }
