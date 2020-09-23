318d843689f1cc310a0535a77aabb8a45afb1e0d
shanyl2400 <white.shan@badanamu.com.cn>
2020-09-23 10:11:46 +0800

    update schema

--- schema/init.sql
+++ schema/init.sql
@@ -39,7 +39,7 @@ CREATE TABLE `cms_contents` (
     FULLTEXT INDEX `content_description_index` (`keywords`) WITH PARSER ngram,
     FULLTEXT INDEX `content_keywords_index` (`description`) WITH PARSER ngram,
     FULLTEXT INDEX `content_author_index` (`author_name`) WITH PARSER ngram
-) COMMENT '内容表' DEFAULT CHARSET=utf8mb4_unicode_ci ;
+) COMMENT '内容表' DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci ;
 
 
 CREATE TABLE IF NOT EXISTS `schedules` (
