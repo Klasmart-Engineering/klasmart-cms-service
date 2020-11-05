126ff43450891d4023ab30dc1c044c190108e6f2
peter.yang <peter.yang@badanamu.com.cn>
2020-11-05 10:02:59 +0800

    [feature]add check permission method

--- external/permission.go
+++ external/permission.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"gitlab.badanamu.com.cn/calmisland/chlorine"
+	"gitlab.badanamu.com.cn/calmisland/common-log/log"
 	"gitlab.badanamu.com.cn/calmisland/kidsloop2/config"
 	"gitlab.badanamu.com.cn/calmisland/kidsloop2/entity"
 )
@@ -34,8 +35,43 @@ type AmsPermisionService struct {
 }
 
 func (s AmsPermisionService) HasPermision(ctx context.Context, operator *entity.Operator, permissionName PermissionName) (bool, error) {
-	// TODO: add implement
-	return false, nil
+	request := chlorine.NewRequest(`
+	query(
+		$user_id: ID! 
+		$organization_id: ID!
+		$permission_name: ID!
+	) {
+		user(user_id: $user_id) {
+			membership(organization_id: $organization_id) {
+				checkAllowed(permission_name: $permission_name)
+			}
+		}
+	}`)
+	request.Var("user_id", operator.UserID)
+	request.Var("organization_id", operator.OrgID)
+	request.Var("permission_name", permissionName.String())
+
+	data := &struct {
+		User struct {
+			Membership struct {
+				CheckAllowed bool `json:"checkAllowed"`
+			} `json:"membership"`
+		} `json:"user"`
+	}{}
+
+	response := &chlorine.Response{
+		Data: data,
+	}
+
+	_, err := GetChlorine().Run(ctx, request, response)
+	if err != nil {
+		log.Error(ctx, "check user permission failed",
+			log.Any("operator", operator),
+			log.String("permissionName", permissionName.String()))
+		return false, err
+	}
+
+	return data.User.Membership.CheckAllowed, nil
 }
 
 func (s AmsPermisionService) GetHasPermissionOrganizations(ctx context.Context, operator *entity.Operator, permissionName PermissionName) ([]*Organization, error) {
--- external/permission_name.go
+++ external/permission_name.go
@@ -2,6 +2,10 @@ package external
 
 type PermissionName string
 
+func (p PermissionName) String() string {
+	return string(p)
+}
+
 const (
 	CreateContentPage201 PermissionName = "create_content_page_201"
 )
