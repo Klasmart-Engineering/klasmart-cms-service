ac9650bd97ec24cbd4e8326e5a84c49fe0cf7c62
peter.yang <peter.yang@badanamu.com.cn>
2020-11-05 16:34:24 +0800

    [feature]implement GetHasPermissionOrganizations()

--- external/permission.go
+++ external/permission.go
@@ -75,6 +75,57 @@ func (s AmsPermissionService) HasPermission(ctx context.Context, operator *entit
 }
 
 func (s AmsPermissionService) GetHasPermissionOrganizations(ctx context.Context, operator *entity.Operator, permissionName PermissionName) ([]*Organization, error) {
-	// TODO: add implement
-	return nil, nil
+	request := chlorine.NewRequest(`
+	query(
+		$user_id: ID!
+		$permission_name: ID!
+	){
+		user(user_id: $user_id) {
+			memberships {
+				organization{
+					organization_id
+					organization_name        
+				}
+				checkAllowed(permission_name: $permission_name)
+			}
+		}
+	}`)
+	request.Var("user_id", operator.UserID)
+	request.Var("permission_name", permissionName.String())
+
+	data := &struct {
+		Memberships []struct {
+			Organization struct {
+				OrganizationID   string `json:"organization_id"`
+				OrganizationName string `json:"organization_name"`
+			} `json:"organization"`
+			CheckAllowed bool `json:"checkAllowed"`
+		} `json:"memberships"`
+	}{}
+
+	response := &chlorine.Response{
+		Data: data,
+	}
+
+	_, err := GetChlorine().Run(ctx, request, response)
+	if err != nil {
+		log.Error(ctx, "check user permission failed",
+			log.Any("operator", operator),
+			log.String("permissionName", permissionName.String()))
+		return nil, err
+	}
+
+	orgs := make([]*Organization, 0, len(data.Memberships))
+	for _, membership := range data.Memberships {
+		if !membership.CheckAllowed {
+			continue
+		}
+
+		orgs = append(orgs, &Organization{
+			ID:   membership.Organization.OrganizationID,
+			Name: membership.Organization.OrganizationName,
+		})
+	}
+
+	return orgs, nil
 }
