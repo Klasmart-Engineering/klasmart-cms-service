8ca94061efca3257dcfc97d1a166002c2bd368b1
kwok.guo <kwok.guo@badanamu.com.cn>
2020-11-25 14:52:34 +0800

    feat:remove schedule_teacher

--- model/schedule_test.go
+++ model/schedule_test.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"gitlab.badanamu.com.cn/calmisland/kidsloop2/external"
 	"testing"
 	"time"
 )
@@ -18,31 +17,5 @@ func TestScheduleModel_GetByID(t *testing.T) {
 }
 
 func TestTemp(t *testing.T) {
-	var s1 []*external.Class
-	s1 = append(s1, &external.Class{
-		ID:   "1",
-		Name: "aaaa",
-	})
-	s1 = append(s1, &external.Class{
-		ID:   "2",
-		Name: "bbbb",
-	})
-	var s2 []*external.Class
-	s2 = s1
-	s2 = append(s1, &external.Class{
-		ID:   "3",
-		Name: "cccc",
-	})
-	t.Log("s1:", len(s1))
-	t.Log("s2:", len(s2))
-
-	t.Log("s1:", s1[0].ID)
-	t.Log("s2:", s2[0].ID)
-	s1[0].ID = "10"
-	t.Log("s1:", s1[0].ID)
-	t.Log("s2:", s2[0].ID)
-	s2[2].ID = "11"
-	t.Log("s1:", s1[1].ID)
-	t.Log("s2:", s2[2].ID)
 
 }
