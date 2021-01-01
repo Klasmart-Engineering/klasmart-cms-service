a39837ea13e0203bffebc4cf291eb40ff3c68e6c
robot <robot@kidsloop.cn>
2021-01-01 17:00:41 +0000

    2021-01-02-010001

--- schema/sync/cn_prod_schema.sql
+++ schema/sync/cn_prod_schema.sql
@@ -677,4 +677,4 @@ CREATE TABLE `visibility_settings` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-31 17:00:30
+-- Dump completed on 2021-01-01 17:00:29
