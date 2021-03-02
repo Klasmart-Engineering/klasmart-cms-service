98151c84aea50ff10350ffa9ebac306e75d0ba45
kwok.guo <kwok.guo@badanamu.com.cn>
2021-03-02 10:22:54 +0800

    fix:add schedule feedback api

--- api/schedule_feedback.go
+++ api/schedule_feedback.go
@@ -1,1 +1,30 @@
 package api
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"gitlab.badanamu.com.cn/calmisland/common-log/log"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/constant"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/entity"
+	"strings"
+)
+
+func (s *Server) addScheduleFeedback(c *gin.Context) {
+
+}
+
+func (s *Server) verifyScheduleFeedback(ctx context.Context, input *entity.ScheduleFeedbackAddInput) (ResponseLabel, error) {
+	if strings.TrimSpace(input.UserID) == "" {
+		log.Info(ctx, "user id is required", log.Any("input", input))
+		return GeneralUnknown, constant.ErrInvalidArgs
+	}
+	if strings.TrimSpace(input.ScheduleID) == "" {
+		log.Info(ctx, "schedule id is required", log.Any("input", input))
+		return GeneralUnknown, constant.ErrInvalidArgs
+	}
+	if strings.TrimSpace(input.AssignmentUrl) == "" {
+		log.Info(ctx, "assignment url is required", log.Any("input", input))
+		return GeneralUnknown, constant.ErrInvalidArgs
+	}
+	return "", nil
+}
