8324c738ba295b1b8191a07e6a76af1eab0919b7
robot <robot@kidsloop.cn>
2021-03-06 17:02:30 +0000

    2021-03-07-010001

--- schema/sync/cn_test_schema.sql
+++ schema/sync/cn_test_schema.sql
@@ -772,4 +772,4 @@ CREATE TABLE `visibility_settings` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-05 17:01:14
+-- Dump completed on 2021-03-06 17:01:15
--- schema/sync/uk_prod_schema.sql
+++ schema/sync/uk_prod_schema.sql
@@ -646,4 +646,4 @@ CREATE TABLE `visibility_settings` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-05 17:02:27
+-- Dump completed on 2021-03-06 17:02:28
