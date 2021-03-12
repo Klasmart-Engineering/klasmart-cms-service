1260c762b3e2971c191545712b43b791ef92a489
peter.yang <peter.yang@badanamu.com.cn>
2021-03-12 17:15:39 +0800

    chore(NKL-551): restore our program model and da

---
+++ da/program.go
@@ -0,0 +1,93 @@
+package da
+
+import (
+	"database/sql"
+	"fmt"
+	"gitlab.badanamu.com.cn/calmisland/dbo"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/entity"
+	"sync"
+)
+
+type IProgramDA interface {
+	dbo.DataAccesser
+}
+
+type programDA struct {
+	dbo.BaseDA
+}
+
+var (
+	_programOnce sync.Once
+	_programDA   IProgramDA
+)
+
+func GetProgramDA() IProgramDA {
+	_programOnce.Do(func() {
+		_programDA = &programDA{}
+	})
+	return _programDA
+}
+
+type ProgramCondition struct {
+	IDs       entity.NullStrings
+	OrgType   sql.NullString
+	OrgID     sql.NullString
+	GroupName sql.NullString
+
+	OrderBy ProgramOrderBy
+	Pager   dbo.Pager
+
+	DeleteAt sql.NullInt64
+}
+
+func (c ProgramCondition) GetConditions() ([]string, []interface{}) {
+	var wheres []string
+	var params []interface{}
+
+	if c.IDs.Valid {
+		wheres = append(wheres, fmt.Sprintf("id in (%s)", c.IDs.SQLPlaceHolder()))
+		params = append(params, c.IDs.ToInterfaceSlice()...)
+	}
+
+	if c.OrgType.Valid {
+		wheres = append(wheres, "org_type = ?")
+		params = append(params, c.OrgType.String)
+	}
+
+	if c.GroupName.Valid {
+		wheres = append(wheres, "group_name = ?")
+		params = append(params, c.GroupName.String)
+	}
+
+	if c.DeleteAt.Valid {
+		wheres = append(wheres, "delete_at>0")
+	} else {
+		wheres = append(wheres, "(delete_at=0)")
+	}
+	return wheres, params
+}
+
+func (c ProgramCondition) GetOrderBy() string {
+	return c.OrderBy.ToSQL()
+}
+
+func (c ProgramCondition) GetPager() *dbo.Pager {
+	return &c.Pager
+}
+
+type ProgramOrderBy int
+
+const (
+	ProgramOrderByNameAsc = iota + 1
+)
+
+func NewProgramOrderBy(orderBy string) ProgramOrderBy {
+	return ProgramOrderByNameAsc
+}
+
+func (c ProgramOrderBy) ToSQL() string {
+	switch c {
+	default:
+		return "number desc, name asc"
+	}
+}
---
+++ entity/program.go
@@ -0,0 +1,41 @@
+package entity
+
+import "gitlab.badanamu.com.cn/calmisland/kidsloop2/constant"
+
+type ProgramGroup string
+
+const (
+	ProgramGroupBadaESL   ProgramGroup = "BadaESL"
+	ProgramGroupBadaSTEAM ProgramGroup = "BadaSTEAM"
+)
+
+func (t ProgramGroup) Valid() bool {
+	switch t {
+	case ProgramGroupBadaESL, ProgramGroupBadaSTEAM:
+		return true
+	default:
+		return false
+	}
+}
+
+type Program struct {
+	ID        string       `json:"id" gorm:"column:id;PRIMARY_KEY"`
+	Name      string       `json:"name" gorm:"column:name;type:varchar(255)"`
+	Number    int          `json:"number" gorm:"column:number;type:int"`
+	OrgType   string       `json:"org_type" gorm:"column:org_type;type:varchar(100)"`
+	GroupName ProgramGroup `json:"group_name" gorm:"column:group_name;type:varchar(100)"`
+	CreateID  string       `gorm:"column:create_id;type:varchar(100)"`
+	UpdateID  string       `gorm:"column:update_id;type:varchar(100)"`
+	DeleteID  string       `gorm:"column:delete_id;type:varchar(100)"`
+	CreateAt  int64        `gorm:"column:create_at;type:bigint"`
+	UpdateAt  int64        `gorm:"column:update_at;type:bigint"`
+	DeleteAt  int64        `gorm:"column:delete_at;type:bigint"`
+}
+
+func (e Program) TableName() string {
+	return constant.TableNameProgram
+}
+
+func (e Program) GetID() interface{} {
+	return e.ID
+}
