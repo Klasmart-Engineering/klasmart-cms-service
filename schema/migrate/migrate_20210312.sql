2a95d4724e470bd787640ff0daeece9aea2eefa5
peter.yang <peter.yang@badanamu.com.cn>
2021-03-19 09:53:36 +0800

    feat(NKL-551): set vn hq org with right org id

--- schema/migrate/migrate_20210312.sql
+++ schema/migrate/migrate_20210312.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `organizations_properties` ADD COLUMN region varchar(100) NOT NULL DEFAULT '' COMMENT 'region';
 
-insert into organizations_properties(id, type, region) values('6cac91e6-0ef2-4be6-9df7-6ea77f7c1928', 'headquarters', 'vn');
+insert into organizations_properties(id, type, region) values('9d42af2a-d943-4bb7-84d8-9e2e28b0e290', 'headquarters', 'vn');
 update organizations_properties set region = 'global' where id = '10f38ce9-5152-4049-b4e7-6d2e2ba884e6';
