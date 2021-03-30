e42d04e493cb0f1504ffce8af79fac151b9e8a2a
robot <robot@kidsloop.cn>
2021-03-30 17:03:53 +0000

    ci(sync-1): sync mysql migrate

--- schema/sync/uk_prod_schema.sql
+++ schema/sync/uk_prod_schema.sql
@@ -679,4 +679,4 @@ CREATE TABLE `visibility_settings` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-30  1:50:15
+-- Dump completed on 2021-03-30 17:03:51
