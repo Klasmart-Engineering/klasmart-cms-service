193708ca9dd4c4412dbf6e3871da939cec116e4c
white.shan <white.shan@badanamu.com.cn>
2021-03-31 10:40:46 +0800

    feat(NKL-647): check share folder permission

--- model/folder_permission.go
+++ model/folder_permission.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"context"
+	"errors"
 	"sync"
 
 	"gitlab.badanamu.com.cn/calmisland/common-log/log"
@@ -9,6 +10,9 @@ import (
 	"gitlab.badanamu.com.cn/calmisland/kidsloop2/external"
 )
 
+var (
+	ErrUnknownRegion = errors.New("unknown region")
+)
 type IFolderPermissionModel interface {
 	CheckFolderOperatorPermission(ctx context.Context, op *entity.Operator) (bool, error)
 	CheckShareFolderOperatorPermission(ctx context.Context, op *entity.Operator) (bool, error)
@@ -33,18 +37,39 @@ func (s *FolderPermissionModel) CheckShareFolderOperatorPermission(ctx context.C
 		return false, nil
 	}
 
-	permission := external.PublishFeaturedContentForAllHub
-	hasPermission, err := external.GetPermissionServiceProvider().HasOrganizationPermission(ctx, op, permission)
-	if err != nil {
-		log.Error(ctx, "get permission failed", log.Err(err))
-		return false, err
+	switch orgInfo.Region {
+	case entity.Global:
+		permissions := []external.PermissionName{external.PublishFeaturedContentForAllHub, external.PublishFeaturedContentForAllOrgsHub}
+		hasPermission, err := external.GetPermissionServiceProvider().HasOrganizationPermissions(ctx, op, permissions)
+		if err != nil {
+			log.Error(ctx, "get permission failed", log.Err(err))
+			return false, err
+		}
+		//有permission，直接返回
+		//has permission
+		for _,v := range hasPermission {
+			if !v {
+				return false, nil
+			}
+		}
+	case entity.VN:
+		permission := external.PublishFeaturedContentForSpecificOrgsHub
+		hasPermission, err := external.GetPermissionServiceProvider().HasOrganizationPermission(ctx, op, permission)
+		if err != nil {
+			log.Error(ctx, "get permission failed", log.Err(err))
+			return false, err
+		}
+		//有permission，直接返回
+		//has permission
+		if !hasPermission {
+			return false, nil
+		}
+	case entity.UnknownRegion:
+		log.Error(ctx, "unknown region", log.Err(err))
+		return false, ErrUnknownRegion
 	}
-	//有permission，直接返回
-	//has permission
-	if hasPermission {
-		return true, nil
-	}
-	return false, nil
+
+	return true, nil
 }
 
 func (s *FolderPermissionModel) CheckFolderOperatorPermission(ctx context.Context, op *entity.Operator) (bool, error) {
