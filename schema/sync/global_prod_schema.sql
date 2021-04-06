fef922cfd6ac6fe847df0d77287c182787cc0e5f
robot <robot@kidsloop.cn>
2021-04-06 17:02:54 +0000

    ci(sync-1): sync mysql migrate

--- schema/sync/global_prod_schema.sql
+++ schema/sync/global_prod_schema.sql
@@ -777,4 +777,4 @@ CREATE TABLE `visibility_settings` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-05 17:00:48
+-- Dump completed on 2021-04-06 17:00:46
--- schema/sync/global_test_schema.sql
+++ schema/sync/global_test_schema.sql
@@ -540,7 +540,7 @@ CREATE TABLE `outcomes_sets` (
   `delete_at` bigint(20) DEFAULT NULL COMMENT 'deleted_at',
   PRIMARY KEY (`id`),
   UNIQUE KEY `outcome_set_id_delete` (`outcome_id`,`set_id`,`delete_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=447 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='outcomes_sets';
+) ENGINE=InnoDB AUTO_INCREMENT=881 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='outcomes_sets';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -844,4 +844,4 @@ CREATE TABLE `visibility_settings` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-05 17:00:49
+-- Dump completed on 2021-04-06 17:00:46
