b07f7f9193c6f1498cc581666709f62b0a5273c9
white.shan <white.shan@badanamu.com.cn>
2021-04-09 15:49:44 +0800

    refactor(NKL-550): transaction move to model

--- model/contentdata.go
+++ model/contentdata.go
@@ -32,106 +32,11 @@ func (cm *ContentModel) CreateContentData(ctx context.Context, contentType entit
 }
 
 func (c *ContentModel) ConvertContentObj(ctx context.Context, obj *entity.Content, operator *entity.Operator) (*entity.ContentInfo, error) {
-	log.Info(ctx, "Convert content object", log.String("extra", obj.Extra))
-	contentData, err := c.CreateContentData(ctx, obj.ContentType, obj.Data)
+	res, err := c.BatchConvertContentObj(ctx, []*entity.Content{obj}, operator)
 	if err != nil {
-		log.Error(ctx, "CreateContentData",
-			log.Err(err),
-			log.Any("obj", obj))
 		return nil, err
 	}
-	teacherManuals := make([]*entity.TeacherManualFile, 0)
-	if obj.ContentType == entity.ContentTypePlan {
-		planData := contentData.(*LessonData)
-		if len(planData.TeacherManualBatch) > 0 {
-			teacherManuals = planData.TeacherManualBatch
-		} else if planData.TeacherManual != "" {
-			teacherManuals = []*entity.TeacherManualFile{{
-				ID:   planData.TeacherManual,
-				Name: planData.TeacherManualName,
-			}}
-		}
-	}
-
-	subjects := make([]string, 0)
-	developmentals := make([]string, 0)
-	skills := make([]string, 0)
-	ages := make([]string, 0)
-	grades := make([]string, 0)
-	keywords := make([]string, 0)
-	outcomes := make([]string, 0)
-	rejectReason := make([]string, 0)
-	program := ""
-
-	contentProperties, err := da.GetContentPropertyDA().BatchGetByContentIDList(ctx, dbo.MustGetDB(ctx), []string{obj.ID})
-	if err != nil {
-		log.Error(ctx, "BatchGetByContentIDList",
-			log.Err(err),
-			log.String("id", obj.ID))
-		return nil, err
-	}
-	for i := range contentProperties {
-		switch contentProperties[i].PropertyType {
-		case entity.ContentPropertyTypeProgram:
-			program = contentProperties[i].PropertyID
-		case entity.ContentPropertyTypeSubject:
-			subjects = append(subjects, contentProperties[i].PropertyID)
-		case entity.ContentPropertyTypeCategory:
-			developmentals = append(developmentals, contentProperties[i].PropertyID)
-		case entity.ContentPropertyTypeAge:
-			ages = append(ages, contentProperties[i].PropertyID)
-		case entity.ContentPropertyTypeGrade:
-			grades = append(grades, contentProperties[i].PropertyID)
-		case entity.ContentPropertyTypeSubCategory:
-			skills = append(skills, contentProperties[i].PropertyID)
-		}
-	}
-
-	if obj.Keywords != "" {
-		keywords = strings.Split(obj.Keywords, constant.StringArraySeparator)
-	}
-	if obj.Outcomes != "" {
-		outcomes = strings.Split(obj.Outcomes, constant.StringArraySeparator)
-	}
-	if obj.RejectReason != "" {
-		rejectReason = strings.Split(obj.RejectReason, constant.StringArraySeparator)
-	}
-
-	cm := &entity.ContentInfo{
-		ID:                 obj.ID,
-		ContentType:        obj.ContentType,
-		Name:               obj.Name,
-		Program:            program,
-		Subject:            subjects,
-		Category:           developmentals,
-		SubCategory:        skills,
-		Age:                ages,
-		Grade:              grades,
-		Keywords:           keywords,
-		SourceType:         obj.SourceType,
-		SuggestTime:        obj.SuggestTime,
-		RejectReason:       rejectReason,
-		Remark:             obj.Remark,
-		Description:        obj.Description,
-		Thumbnail:          obj.Thumbnail,
-		Data:               obj.Data,
-		Extra:              obj.Extra,
-		Outcomes:           outcomes,
-		Author:             obj.Author,
-		TeacherManualBatch: teacherManuals,
-		Creator:            obj.Creator,
-		SelfStudy:          obj.SelfStudy.Bool(),
-		DrawActivity:       obj.DrawActivity.Bool(),
-		LessonType:         obj.LessonType,
-		Org:                obj.Org,
-		//PublishScope:  visibilitySettings,
-		PublishStatus: obj.PublishStatus,
-		Version:       obj.Version,
-		CreatedAt:     obj.CreateAt,
-		UpdatedAt:     obj.UpdateAt,
-	}
-
-	return cm, nil
+	return res[0], nil
 }
 
 func (c *ContentModel) BatchConvertContentObj(ctx context.Context, objs []*entity.Content, operator *entity.Operator) ([]*entity.ContentInfo, error) {
