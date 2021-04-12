1ddff80c6b6c7e87709ebba07c7c90d11c95587f
peter.yang <peter.yang@badanamu.com.cn>
2021-04-12 13:39:40 +0800

    chore(NKL-755): lint free

--- cmd/intergrate_academic_profile/age.go
+++ cmd/intergrate_academic_profile/age.go
@@ -118,7 +118,7 @@ func (s *MapperImpl) loadOurAges(ctx context.Context) error {
 	return nil
 }
 
-func (s MapperImpl) defaultAge(programID string) (string, error) {
+func (s *MapperImpl) defaultAge(programID string) (string, error) {
 	switch programID {
 	case "7565ae11-8130-4b7d-ac24-1d9dd6f792f2":
 		// None Specified
