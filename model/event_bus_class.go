36638c1726a1045c88673cbeec260ef02debe5fb
kwok.guo <kwok.guo@badanamu.com.cn>
2021-04-13 00:02:22 +0800

    feat(NKL-664): add event bus model

--- model/event_bus_class.go
+++ model/event_bus_class.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"context"
 	"sync"
+
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/entity"
 )
 
 const (
@@ -9,17 +12,36 @@ const (
 	BusTopicClassDeleteMembers BusTopic = "ClassDeleteMembers"
 )
 
+type BusTopicClassAddMembersFunc func(ctx context.Context, op *entity.Operator, event *entity.ScheduleClassEvent) error
+
+type IClassEventBus interface {
+	SubAddMembers(handler BusTopicClassAddMembersFunc) error
+	PubAddMembers(ctx context.Context, op *entity.Operator, event *entity.ScheduleClassEvent) error
+}
+
+type classEventBus struct {
+	bus *AsyncEventBus
+}
+
+func (b *classEventBus) SubAddMembers(handler BusTopicClassAddMembersFunc) error {
+	return b.bus.Sub(BusTopicClassAddMembers, handler)
+}
+
+func (b *classEventBus) PubAddMembers(ctx context.Context, op *entity.Operator, event *entity.ScheduleClassEvent) error {
+	return b.bus.Pub(BusTopicClassAddMembers, ctx, op, event)
+}
+
 var (
 	_busEventOnce sync.Once
-	_busModel     IBus
+	_busModel     IClassEventBus
 )
 
-func GetClassEventBusModel() IBus {
+func GetClassEventBusModel() IClassEventBus {
 	_busEventOnce.Do(func() {
-		bus := NewAsyncEventBus()
-		bus.Sub(BusTopicClassAddMembers, GetScheduleEventModel().AddUserEvent)
-		bus.Sub(BusTopicClassDeleteMembers, GetScheduleEventModel().DeleteUserEvent)
-
+		bus := &classEventBus{
+			bus: NewAsyncEventBus(),
+		}
+		bus.SubAddMembers(GetScheduleEventModel().AddUserEvent)
 		_busModel = bus
 	})
 	return _busModel
