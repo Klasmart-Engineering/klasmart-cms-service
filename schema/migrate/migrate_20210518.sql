3fec56af9deade57c9e400f89e59662845230307
Medivh <ext.zhu.yang@badanamu.com.cn>
2021-05-18 16:26:57 +0800

    feat(NKL-878): update migrate sql

--- schema/migrate/migrate_20210518.sql
+++ schema/migrate/migrate_20210518.sql
@@ -1,2 +1,2 @@
 ALTER TABLE assessments ADD `type` VARCHAR(1024) NOT NULL DEFAULT '' COMMENT 'assessment type';
-ALTER TABLE assessments_contents ADD `content_source` TEXT NOT NULL DEFAULT '' COMMENT 'content source';
+UPDATE assessments SET type = 'class_and_live_outcome' where 'type' = '';
