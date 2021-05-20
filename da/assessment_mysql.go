523cb492f069b624cbca3b60bc6020f64878287d
Medivh <ext.zhu.yang@badanamu.com.cn>
2021-05-20 17:24:29 +0800

    fix(NKL-878): order by

--- da/assessment_mysql.go
+++ da/assessment_mysql.go
@@ -278,9 +278,9 @@ func (c *QueryAssessmentConditions) GetOrderBy() string {
 	case entity.ListAssessmentsOrderByClassEndTimeDesc:
 		return "class_end_time desc"
 	case entity.ListAssessmentsOrderByCompleteTime:
-		return "complete_time"
+		return "status desc, complete_time, create_at"
 	case entity.ListAssessmentsOrderByCompleteTimeDesc:
-		return "complete_time desc"
+		return "status desc, complete_time desc, create_at desc"
 	case entity.ListAssessmentsOrderByCreateAt:
 		return "create_at"
 	case entity.ListAssessmentsOrderByCreateAtDesc:
