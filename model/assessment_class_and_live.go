e3ad32bf73baa30eaa730985dec2e5283644ab19
Medivh <ext.zhu.yang@badanamu.com.cn>
2021-06-03 15:07:28 +0800

    fix(NKL-945): compile

--- model/assessment_class_and_live.go
+++ model/assessment_class_and_live.go
@@ -218,7 +218,7 @@ func (m *classAndLiveAssessmentModel) GetDetail(ctx context.Context, tx *dbo.DBC
 	result.RemainingTime = int64(GetAssessmentUtils().CalcRemainingTime(view.Schedule.DueAt, view.CreateAt).Seconds())
 
 	// fill student view items
-	result.StudentViewItems, err = GetAssessmentUtils().GetH5PStudentViewItems(ctx, operator, view)
+	result.StudentViewItems, err = GetAssessmentUtils().GetH5PStudentViewItems(ctx, operator, tx, view)
 	if err != nil {
 		log.Error(ctx, "get assessment detail: get student view items failed",
 			log.Err(err),
