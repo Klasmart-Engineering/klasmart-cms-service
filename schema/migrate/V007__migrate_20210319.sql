789df4084877751020bed5eb8e2edc7cbc5e7633
peter.yang <peter.yang@badanamu.com.cn>
2021-06-12 10:59:47 +0800

    feat(NKL-966): remove old migrate scripts

--- schema/migrate/V007__migrate_20210319.sql
+++
@@ -1,5 +0,0 @@
-CREATE UNIQUE INDEX uq_home_fun_studies_schedule_id_and_student_id ON home_fun_studies(schedule_id, student_id);
-ALTER TABLE home_fun_studies RENAME INDEX `home_fun_study_id` TO `idx_home_fun_studies_schedule_id`;
-ALTER TABLE home_fun_studies RENAME INDEX `home_fun_study_status` TO `idx_home_fun_studies_status`;
-ALTER TABLE home_fun_studies RENAME INDEX `home_fun_study_latest_feedback_at` TO `idx_home_fun_studies_latest_feedback_at`;
-ALTER TABLE home_fun_studies RENAME INDEX `home_fun_study_complete_at` TO `idx_home_fun_studies_complete_at`;
--- schema/migrate/V010__migrate_20210326.sql
+++
@@ -1,1 +0,0 @@
-insert into programs_groups(program_id, group_name) values ("7565ae11-8130-4b7d-ac24-1d9dd6f792f2", "More");
