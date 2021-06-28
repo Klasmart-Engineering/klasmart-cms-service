97cb5eb222f865f29bff9f525f20744cfdf8f0de
Medivh <ext.zhu.yang@badanamu.com.cn>
2021-06-28 17:00:37 +0800

    fix(NKL-972): check empty content id

--- model/assessment_h5p.go
+++ model/assessment_h5p.go
@@ -181,6 +181,12 @@ func (m *assessmentH5P) batchGetRoomScoreMap(ctx context.Context, operator *enti
 			sort.Slice(u.Scores, func(i, j int) bool {
 				itemI := u.Scores[i]
 				itemJ := u.Scores[j]
+				if itemI.Content == nil {
+					return true
+				}
+				if itemJ.Content == nil {
+					return false
+				}
 				if itemI.Content.ContentID == itemJ.Content.ContentID {
 					if itemI.Content.SubContentID == "" && itemJ.Content.SubContentID != "" {
 						return true
