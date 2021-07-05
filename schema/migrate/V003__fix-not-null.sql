fb4ae2085261caf8d7053670a1e016e148558b6e
white.shan <white.shan@badanamu.com.cn>
2021-07-05 13:34:12 +0800

    fix(NKL-1060): fix sql

--- schema/migrate/V003__fix-not-null.sql
+++ schema/migrate/V003__fix-not-null.sql
@@ -1,15 +1,38 @@
- ALTER  TABLE cms_contents MODIFY 
- `program` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'program',
-  `subject` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'subject',
-  `developmental` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'developmental',
-  `skills` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'skills',
-  `age` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'age',
-  `grade` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'grade'
-
- ALTER  TABLE learning_outcomes MODIFY 
-  `program` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'program',
-  `subject` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'subject',
-  `developmental` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'developmental',
-  `skills` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'skills',
-  `age` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'age',
-  `grade` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'grade',
+-- cms_contents
+ALTER TABLE cms_contents MODIFY 
+ `program` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'program';
+ 
+ALTER TABLE cms_contents MODIFY 
+ `subject` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'subject';
+ 
+ALTER TABLE cms_contents MODIFY 
+ `developmental` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'developmental';
+ 
+ ALTER TABLE cms_contents MODIFY 
+ `skills` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'skills';
+ 
+ ALTER TABLE cms_contents MODIFY 
+ `age` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'age';
+ 
+ ALTER TABLE cms_contents MODIFY 
+ `grade` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'grade';
+ 
+ 
+-- learning_outcomes
+ ALTER TABLE learning_outcomes MODIFY 
+ `program` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'program';
+ 
+ALTER TABLE learning_outcomes MODIFY 
+ `subject` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'subject';
+ 
+ALTER TABLE learning_outcomes MODIFY 
+ `developmental` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'developmental';
+ 
+ ALTER TABLE learning_outcomes MODIFY 
+ `skills` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'skills';
+ 
+ ALTER TABLE learning_outcomes MODIFY 
+ `age` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'age';
+ 
+ ALTER TABLE learning_outcomes MODIFY 
+ `grade` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'grade';
