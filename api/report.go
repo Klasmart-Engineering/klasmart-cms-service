b59f6143ae8b9713e54bb5a21b7b8e32aa7a826c
peter.yang <peter.yang@badanamu.com.cn>
2021-07-20 16:13:02 +0800

    chore(NKL-824): remove deprecated h5p events and student reports

--- api/report.go
+++ api/report.go
@@ -2,6 +2,9 @@ package api
 
 import (
 	"database/sql"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"gitlab.badanamu.com.cn/calmisland/common-log/log"
 	"gitlab.badanamu.com.cn/calmisland/dbo"
@@ -9,8 +12,6 @@ import (
 	"gitlab.badanamu.com.cn/calmisland/kidsloop2/entity"
 	"gitlab.badanamu.com.cn/calmisland/kidsloop2/model"
 	"gitlab.badanamu.com.cn/calmisland/kidsloop2/utils"
-	"net/http"
-	"strconv"
 )
 
 // @Summary list student report
@@ -189,83 +190,6 @@ func (s *Server) getStudentPerformanceReport(ctx *gin.Context) {
 		Operator:     operator,
 	}
 	result, err := model.GetReportModel().GetStudentPerformanceReport(reqCtx, dbo.MustGetDB(reqCtx), operator, req)
-	switch err {
-	case nil:
-		ctx.JSON(http.StatusOK, result)
-	case constant.ErrInvalidArgs:
-		ctx.JSON(http.StatusBadRequest, L(GeneralUnknown))
-	case constant.ErrRecordNotFound, sql.ErrNoRows:
-		ctx.JSON(http.StatusNotFound, L(GeneralUnknown))
-	case constant.ErrForbidden:
-		ctx.JSON(http.StatusForbidden, L(ReportMsgNoPermission))
-	default:
-		ctx.JSON(http.StatusInternalServerError, L(GeneralUnknown))
-	}
-}
-
-// @Summary list student performance h5p report
-// @Description list student performance h5p report
-// @Tags reports
-// @ID listStudentsPerformanceH5PReport
-// @Accept json
-// @Produce json
-// @Param teacher_id query string true "teacher_id"
-// @Param class_id query string true "class_id"
-// @Param lesson_plan_id query string true "lesson plan id"
-// @Success 200 {object} entity.ListStudentsPerformanceH5PReportResponse
-// @Failure 400 {object} BadRequestResponse
-// @Failure 403 {object} ForbiddenResponse
-// @Failure 500 {object} InternalServerErrorResponse
-// @Router /reports/performance/h5p/students [get]
-func (s *Server) listStudentsPerformanceH5PReport(ctx *gin.Context) {
-	reqCtx := ctx.Request.Context()
-	operator := s.getOperator(ctx)
-	req := entity.ListStudentsPerformanceH5PReportRequest{
-		TeacherID:    ctx.Query("teacher_id"),
-		ClassID:      ctx.Query("class_id"),
-		LessonPlanID: ctx.Query("lesson_plan_id"),
-		Operator:     operator,
-	}
-	result, err := model.GetReportModel().ListStudentsPerformanceH5PReport(reqCtx, dbo.MustGetDB(reqCtx), operator, req)
-	switch err {
-	case nil:
-		ctx.JSON(http.StatusOK, result)
-	case constant.ErrInvalidArgs:
-		ctx.JSON(http.StatusBadRequest, L(GeneralUnknown))
-	case constant.ErrForbidden:
-		ctx.JSON(http.StatusForbidden, L(ReportMsgNoPermission))
-	default:
-		ctx.JSON(http.StatusInternalServerError, L(GeneralUnknown))
-	}
-}
-
-// @Summary get student performance h5p report
-// @Description get student performance h5p report
-// @Tags reports
-// @ID getStudentPerformanceH5PReport
-// @Accept json
-// @Produce json
-// @Param id path string true "student id"
-// @Param teacher_id query string true "teacher_id"
-// @Param class_id query string true "class_id"
-// @Param lesson_plan_id query string true "lesson plan id"
-// @Success 200 {object} entity.GetStudentPerformanceH5PReportResponse
-// @Failure 400 {object} BadRequestResponse
-// @Failure 403 {object} ForbiddenResponse
-// @Failure 404 {object} NotFoundResponse
-// @Failure 500 {object} InternalServerErrorResponse
-// @Router /reports/performance/h5p/students/{id} [get]
-func (s *Server) getStudentPerformanceH5PReport(ctx *gin.Context) {
-	reqCtx := ctx.Request.Context()
-	operator := s.getOperator(ctx)
-	req := entity.GetStudentPerformanceH5PReportRequest{
-		StudentID:    ctx.Param("id"),
-		TeacherID:    ctx.Query("teacher_id"),
-		ClassID:      ctx.Query("class_id"),
-		LessonPlanID: ctx.Query("lesson_plan_id"),
-		Operator:     operator,
-	}
-	result, err := model.GetReportModel().GetStudentPerformanceH5PReport(reqCtx, dbo.MustGetDB(reqCtx), operator, req)
 	switch err {
 	case nil:
 		ctx.JSON(http.StatusOK, result)
