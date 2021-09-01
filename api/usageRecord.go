e2745a73f6ec6b2f821d13e4063c7b660a4fedd7
brilliant.yang <brilliant.yang@kidsloop.cn>
2021-09-01 17:57:05 +0800

    feat(NKL-1238): student usage record swagger

--- api/usageRecord.go
+++ api/usageRecord.go
@@ -5,15 +5,12 @@ import (
 )
 
 type StudentUsageRecord struct {
-	ClassType      string `json:"class_type"`
-	RoomID         string `json:"room_id"`
-	LessonMaterial struct {
-		Url  string `json:"url"`
-		Type string `json:"type" enums:"Iframe,Video,Audio,Image"`
-	} `json:"lesson_material"`
-	ContentType string `json:"h5p_type" enums:"h5p, audio, video, image, document"`
-	ActionType  string `json:"action_type" enums:"view"`
-	Timestamp   int64  `json:"timestamp"`
+	ClassType         string `json:"class_type"`
+	RoomID            string `json:"room_id"`
+	LessonMaterialUrl string `json:"lesson_material_url"`
+	ContentType       string `json:"h5p_type" enums:"h5p, audio, video, image, document"`
+	ActionType        string `json:"action_type" enums:"view"`
+	Timestamp         int64  `json:"timestamp"`
 }
 
 // @Summary student usage record
