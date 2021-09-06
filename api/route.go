fe15fb1192ba3bfe2240bfa5176119f04884350b
brilliant.yang <brilliant.yang@kidsloop.cn>
2021-09-06 17:33:41 +0800

    feat(NKL-1238): swagger

--- api/route.go
+++ api/route.go
@@ -156,6 +156,9 @@ func (s Server) registeRoute() {
 		reports.GET("/reports/learning_summary/assignments", s.mustLogin, s.queryAssignmentsSummary)
 
 		reports.GET("/reports/student_usage/material", s.mustLogin, s.getStudentUsageMaterialReport)
+		reports.GET("/reports/student_usage/classes_assignments_overview", s.mustLogin, s.getClassesAssignmentsOverview)
+		reports.GET("/reports/student_usage/classes_assignments", s.mustLogin, s.getClassesAssignments)
+		reports.GET("/reports/student_usage/classes_assignments/:class_id/unattended", s.mustLogin, s.getClassesAssignmentsUnattended)
 	}
 
 	outcomes := s.engine.Group("/v1")
--- entity/report.go
+++ entity/report.go
@@ -485,3 +485,31 @@ type ContentUsage struct {
 	Type  string `json:"type"`
 	Count int    `json:"count"`
 }
+
+type ClassesAssignmentCount struct {
+	Type  string `json:"type"`
+	Count int    `json:"count"`
+}
+type ClassesAssignmentsOverView []ClassesAssignmentCount
+
+type ClassesAssignmentsDurationRatio struct {
+	Key   string  `json:"key"`
+	Ratio float32 `json:"ratio"`
+}
+type ClassesAssignmentDetail struct {
+	ClassID        string                            `json:"class_id"`
+	Total          int                               `json:"total"`
+	DurationsRatio []ClassesAssignmentsDurationRatio `json:"durations_ratio"`
+}
+
+type ClassesAssignmentsUnattended struct {
+	StudentID string `json:"student_id"`
+	Schedule  struct {
+		ScheduleID   string `json:"schedule_id"`
+		ScheduleName string `json:"schedule_name"`
+		Type         string `json:"type"`
+	} `json:"schedule"`
+	Time int64 `json:"time"`
+}
+
+type ClassesAssignmentsUnattendedStudentsView []ClassesAssignmentsUnattended
