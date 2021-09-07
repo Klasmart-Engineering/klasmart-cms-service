3969ff1c6abca268df340978023a4e499a2c0c2e
brilliant.yang <brilliant.yang@kidsloop.cn>
2021-09-07 11:24:37 +0800

    feat(NKL-1246): classes assignments

--- entity/report_classes_assignments.go
+++ entity/report_classes_assignments.go
@@ -1,8 +1,18 @@
 package entity
 
+type ClassesAssignmentOverViewRequest struct {
+	ClassIDs  []string    `json:"class_ids" form:"class_ids"`
+	Durations []TimeRange `json:"durations" form:"durations"`
+}
+
 type ClassesAssignmentOverView struct {
 	Type  string `json:"type"`
 	Count int    `json:"count"`
+}
+
+type ClassesAssignmentsViewRequest struct {
+	ClassIDs  []string    `json:"class_ids" form:"class_ids"`
+	Durations []TimeRange `json:"durations" form:"durations"`
 }
 
 type ClassesAssignmentsDurationRatio struct {
@@ -16,6 +26,13 @@ type ClassesAssignmentsView struct {
 	DurationsRatio []ClassesAssignmentsDurationRatio `json:"durations_ratio"`
 }
 
+type ClassesAssignmentsUnattendedStudentsViewRequest struct {
+	ClassID   string
+	Page      int         `json:"page" form:"page"`
+	PageSize  int         `json:"page_size" form:"page_size"`
+	Durations []TimeRange `json:"durations" form:"durations"`
+}
+
 type ClassesAssignmentsUnattendedStudentsView struct {
 	StudentID string `json:"student_id"`
 	Schedule  struct {
@@ -24,4 +41,17 @@ type ClassesAssignmentsUnattendedStudentsView struct {
 		Type         string `json:"type"`
 	} `json:"schedule"`
 	Time int64 `json:"time"`
+}
+
+type ClassesAssignmentsRecords struct {
+	ID              string `json:"id"`
+	ClassID         string `json:"class_id"`
+	ScheduleID      string `json:"schedule_id"`
+	AttendanceID    string `json:"attendance_id"`
+	ScheduleType    string `json:"schedule_type"`
+	ScheduleStartAt int64  `json:"schedule_start_at"`
+}
+
+func (ClassesAssignmentsRecords) TableName() string {
+	return "classes_assignments_records"
 }
