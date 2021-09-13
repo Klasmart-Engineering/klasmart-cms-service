f1cd98fd1ce5383aae9c6042d6d4c27000cfad85
brilliant.yang <brilliant.yang@kidsloop.cn>
2021-09-13 15:52:46 +0800

    feat(NKL-1247): parent folder for contents

--- schema/migrate/V017__add_parent_folder_for_content.sql
+++ schema/migrate/V017__add_parent_folder_for_content.sql
@@ -3,4 +3,4 @@ alter table cms_contents add parent_folder varchar(50) CHARACTER SET utf8mb4 COL
 update cms_contents
 set
     parent_folder=if(dir_path='/' or dir_path='', '/', substring_index(dir_path, '/', -1)),
-    extra=concat(extra, if(extra is null or extra='', '', ';'), 'parent_folder_0903');
+    extra=concat(extra, if(extra is null or extra='', '', ';'), 'parent_folder_0913');
