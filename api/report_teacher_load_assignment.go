9eabf2596075cd934f34455e20f17149715fd4a7
Chuanbing <chuanbing.lu@badanamu.com.cn>
2021-09-22 15:31:55 +0800

    feat(NKL-1296):  define teacher report assignment

---
+++ api/report_teacher_load_assignment.go
@@ -0,0 +1,52 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gitlab.badanamu.com.cn/calmisland/common-log/log"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/constant"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/entity"
+)
+
+// @Summary list teaching load report
+// @Description list teaching load report
+// @Tags reports
+// @ID getTeacherLoadReportOfAssignment
+// @Accept json
+// @Produce json
+// @Param request body string entity.TeacherLoadAssignmentRequest "request arg"
+// @Success 200 {object} entity.TeacherLoadAssignmentResponse
+// @Failure 400 {object} BadRequestResponse
+// @Failure 403 {object} ForbiddenResponse
+// @Failure 500 {object} InternalServerErrorResponse
+// @Router /reports/teacher_report_assignment [get]
+func (s *Server) getTeacherLoadReportOfAssignment(c *gin.Context) {
+	ctx := c.Request.Context()
+	var err error
+	defer func() {
+		if err == nil {
+			return
+		}
+		switch err {
+		case constant.ErrInvalidArgs:
+			c.JSON(http.StatusBadRequest, L(GeneralUnknown))
+		default:
+			s.defaultErrorHandler(c, err)
+		}
+	}()
+	op := s.getOperator(c)
+	req := entity.TeacherLoadAssignmentRequest{}
+	err = c.ShouldBindJSON(&req)
+	if err != nil {
+		log.Error(ctx, "invalid request", log.Err(err))
+		err = constant.ErrInvalidArgs
+		return
+	}
+	//res, err := model.GetReportModel().GetStudentUsageMaterialViewCount(ctx, op, &req)
+	//if err != nil {
+	//	return
+	//}
+	_ = op
+	c.JSON(http.StatusOK, entity.TeacherLoadAssignmentResponse{})
+}
