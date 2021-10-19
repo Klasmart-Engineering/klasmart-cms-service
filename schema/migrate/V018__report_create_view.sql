3a6222ae01a5d8c6ca348fa8594c7a02f0ca48da
Chuanbing <chuanbing.lu@badanamu.com.cn>
2021-10-19 11:08:07 +0800

    fix(NKL-1349): update swagger

--- schema/migrate/V018__report_create_view.sql
+++ schema/migrate/V018__report_create_view.sql
@@ -3,13 +3,12 @@
 -- return n rows if schedule has n subject, and subject_id has value
 create or replace view v_schedules_subjects as
 (
-select s.id                                           as schedule_id,
-       sr1.relation_id                                as class_id,
-       if(sr.relation_id is null, '', sr.relation_id) as subject_id
-from schedules s
-         left join schedules_relations sr on s.id = sr.schedule_id and sr.relation_type = 'Subject'
-         left join schedules_relations sr1 on s.id = sr1.schedule_id and sr1.relation_type = 'class_roster_class'
-where s.delete_at = 0
+select sr.schedule_id,
+       sr.relation_id                                as class_id,
+       if(sr1.relation_id is null, '', sr1.relation_id) as subject_id
+from   schedules_relations sr
+         left join schedules_relations sr1 on sr.schedule_id = sr1.schedule_id and sr1.relation_type = 'Subject'
+where   sr.relation_type = 'class_roster_class'
 );
 
 -- return m*n rows, if m students should achieve n outcomes
@@ -42,3 +41,15 @@ from
             on t1.assessment_id = oa.assessment_id
             and t1.outcome_id = oa.outcome_id
             and t2.student_id = oa.attendance_id;
+
+-- return the time student first achieve outcome
+create or replace view  v_students_outcomes_first_achieve as
+select
+    vaos.student_id,vaos.outcome_id,
+    min(a.complete_time) as first_achieve_time
+from
+    v_assessments_outcomes_students vaos
+        inner join assessments a on
+            a.id = vaos.assessment_id
+where a.status ='complete' and vaos.is_student_achieved=1
+group by vaos.student_id,vaos.outcome_id;
