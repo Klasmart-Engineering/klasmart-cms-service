92f57230701b0013785c18f3fc816a4a8efd6d22
brilliant.yang <brilliant.yang@kidsloop.cn>
2021-10-19 11:32:38 +0800

    feat(NKL-1350): api

--- api/report_student_progress_assignment_completion.go
+++ api/report_student_progress_assignment_completion.go
@@ -1,6 +1,12 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gitlab.badanamu.com.cn/calmisland/common-log/log"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/entity"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/model"
+	"net/http"
+)
 
 // @Summary  getAssignmentsCompletion
 // @Tags reports/studentProgress
@@ -14,5 +20,24 @@ import "github.com/gin-gonic/gin"
 // @Failure 500 {object} InternalServerErrorResponse
 // @Router /reports/student_progress/assignment_completion [post]
 func (s *Server) getAssignmentsCompletion(c *gin.Context) {
-	panic("implement me")
+
+	ctx := c.Request.Context()
+	op := s.getOperator(c)
+
+	var request entity.AssignmentRequest
+	err := c.ShouldBindJSON(&request)
+	if err != nil {
+		log.Error(ctx, "getAssignmentsCompletion: ShouldBindQuery failed",
+			log.Err(err),
+			log.Any("request", request))
+		c.JSON(http.StatusBadRequest, L(GeneralUnknown))
+		return
+	}
+	result, err := model.GetReportModel().GetAssignmentCompletion(ctx, op, &request)
+	switch err {
+	case nil:
+		c.JSON(http.StatusOK, result)
+	default:
+		s.defaultErrorHandler(c, err)
+	}
 }
