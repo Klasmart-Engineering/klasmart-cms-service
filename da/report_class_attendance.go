9f3e95e720028fa757276c234068e90f10679dae
allen <allen.shen@badamuna.com.cn>
2021-10-21 17:14:00 +0800

    fix(NKL-1349): optimize  class attendacne code

--- da/report_class_attendance.go
+++ da/report_class_attendance.go
@@ -1,10 +1,11 @@
 package da
 
 import (
+	"bytes"
 	"context"
 	"gitlab.badanamu.com.cn/calmisland/common-log/log"
 	"gitlab.badanamu.com.cn/calmisland/kidsloop2/entity"
-	"strings"
+	"text/template"
 )
 
 type IClassAttendance interface {
@@ -25,7 +26,7 @@ from
             on sr.schedule_id = sr1.schedule_id
             inner join schedules sh
             on sr.schedule_id = sh.id
-            where sr.relation_type = '${class_roster_class}' and sr1.relation_type = '${subject}'
+            where sr.relation_type = '{{.ClassRosterClass}}' and sr1.relation_type = '{{.Subject}}'
     )
 as s1
 inner join
@@ -35,24 +36,47 @@ inner join
     left join classes_assignments_records  car
     on sr.schedule_id=car.schedule_id
     and sr.relation_id=car.attendance_id
-    where sr.relation_type='${class_roster_student}'
+    where sr.relation_type='{{.ClassRosterStudent}}'
    )
 as s2
 on s1.schedule_id=s2.schedule_id
 and s1.class_id=s2.class_id
-and s1.class_type='${OnlineClass}'
+and s1.class_type='{{.OnlineClass}}'
 where  s1.class_id=? and s1.start_at>=? and s1.start_at<? and s1.subject_id in (?)
 order by s1.class_id,s1.subject_id
 `
-	sql = strings.Replace(sql, "${subject}", entity.ScheduleClassTypeSubject.String(), -1)
-	sql = strings.Replace(sql, "${OnlineClass}", entity.ScheduleClassTypeOnlineClass.String(), -1)
-	sql = strings.Replace(sql, "${class_roster_student}", entity.ScheduleRelationTypeClassRosterStudent.String(), -1)
-	sql = strings.Replace(sql, "${class_roster_class}", entity.ScheduleRelationTypeClassRosterClass.String(), -1)
+	sqlTmplParameter := struct {
+		ClassRosterClass   string
+		Subject            string
+		ClassRosterStudent string
+		OnlineClass        string
+	}{
+		ClassRosterClass:   entity.ScheduleRelationTypeClassRosterClass.String(),
+		Subject:            entity.ScheduleClassTypeSubject.String(),
+		ClassRosterStudent: entity.ScheduleRelationTypeClassRosterStudent.String(),
+		OnlineClass:        entity.ScheduleClassTypeOnlineClass.String(),
+	}
+	sqlTmpl, err := template.New("sql").Parse(sql)
+	if err != nil {
+		log.Error(ctx, "create GetClassAttendance sql tmpl failed",
+			log.Err(err),
+			log.String("sql", sql))
+		return
+	}
+	buffer := new(bytes.Buffer)
+	err = sqlTmpl.Execute(buffer, sqlTmplParameter)
+	if err != nil {
+		log.Error(ctx, " GetClassAttendance replace sqlTmpl failed",
+			log.Err(err),
+			log.String("sql", sql),
+			log.Any("params", sqlTmplParameter))
+		return
+	}
 	startAt, endAt, err := request.Duration.Value(ctx)
 	if err != nil {
 		return
 	}
-	err = r.QueryRawSQL(ctx, &model, sql, request.ClassID, startAt, endAt, request.SubjectIDS)
+	err = r.QueryRawSQL(ctx, &model, buffer.String(), request.ClassID, startAt, endAt, request.SubjectIDS)
 	if err != nil {
 		log.Error(ctx, "exec GetClassAttendance sql failed",
 			log.Err(err),
--- model/report_class_attendance.go
+++ model/report_class_attendance.go
@@ -4,19 +4,12 @@ import (
 	"context"
 	"gitlab.badanamu.com.cn/calmisland/kidsloop2/da"
 	"gitlab.badanamu.com.cn/calmisland/kidsloop2/entity"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/utils"
 )
-
-var classSelectSubjectCount int
-var classSelectSubjectAttendanceCount int
-var studentSelectSubjectCount int
-var studentSelectSubjectAttendanceCount int
-var studentUnSelectSubjectCount int
-var studentUnSelectSubjectAttendanceCount int
 
 func (m *reportModel) ClassAttendanceStatistics(ctx context.Context, request *entity.ClassAttendanceRequest) (response *entity.ClassAttendanceResponse, err error) {
 	response = new(entity.ClassAttendanceResponse)
 	classAttendanceResponseItem := new(entity.ClassAttendanceResponseItem)
-	reportDA := da.GetReportDA()
 	response.RequestStudentID = request.StudentID
 	response.Items = append(response.Items, classAttendanceResponseItem)
 	for _, duration := range request.Durations {
@@ -24,33 +17,37 @@ func (m *reportModel) ClassAttendanceStatistics(ctx context.Context, request *en
 		queryParameters.ClassID = request.ClassID
 		queryParameters.Duration = duration
 		// query condition all subject
-		subjectList := make([]string, 0)
-		subjectList = request.SelectedSubjectIDList
-		for _, subject := range request.UnSelectedSubjectIDList {
-			subjectList = append(subjectList, subject)
-		}
-		queryParameters.SubjectIDS = subjectList
-		classAttendanceList, err := reportDA.GetClassAttendance(ctx, queryParameters)
+		queryParameters.SubjectIDS = append(request.SelectedSubjectIDList, request.UnSelectedSubjectIDList...)
+		classAttendanceList, err := da.GetReportDA().GetClassAttendance(ctx, queryParameters)
 		if err != nil {
 			return nil, err
 		}
+		var classSelectSubjectCount int
+		var classSelectSubjectAttendanceCount int
+		var studentSelectSubjectCount int
+		var studentSelectSubjectAttendanceCount int
+		var studentUnSelectSubjectCount int
+		var studentUnSelectSubjectAttendanceCount int
 		for _, classAttendance := range classAttendanceList {
-			if isExistInArray(classAttendance.SubjectID, request.SelectedSubjectIDList) {
+			// statistics class average attendance selected subject
+			if utils.ContainsStr(request.SelectedSubjectIDList, classAttendance.SubjectID) {
 				classSelectSubjectCount++
 				if classAttendance.IsAttendance {
 					classSelectSubjectAttendanceCount++
 				}
 			}
+			//statistics student attendance selected subject
 			if classAttendance.StudentID == request.StudentID &&
-				isExistInArray(classAttendance.SubjectID, request.SelectedSubjectIDList) {
+				utils.ContainsStr(request.SelectedSubjectIDList, classAttendance.SubjectID) {
 				studentSelectSubjectCount++
 				if classAttendance.IsAttendance {
 					studentSelectSubjectAttendanceCount++
 				}
 			}
+			//when subject select all,don't statistics student attendance other subject
 			if len(request.UnSelectedSubjectIDList) == 0 {
 				if classAttendance.StudentID == request.StudentID &&
-					isExistInArray(classAttendance.SubjectID, request.UnSelectedSubjectIDList) {
+					utils.ContainsStr(request.UnSelectedSubjectIDList, classAttendance.SubjectID) {
 					studentUnSelectSubjectCount++
 					if classAttendance.IsAttendance {
 						studentUnSelectSubjectAttendanceCount++
@@ -61,34 +58,15 @@ func (m *reportModel) ClassAttendanceStatistics(ctx context.Context, request *en
 		classAttendanceResponseItem.Duration = duration
 		classAttendanceResponseItem.AttendedCount = studentSelectSubjectAttendanceCount + studentUnSelectSubjectAttendanceCount
 		classAttendanceResponseItem.ScheduledCount = studentSelectSubjectCount + studentUnSelectSubjectCount
-		if len(request.UnSelectedSubjectIDList) == 0 {
-			if studentSelectSubjectCount > 0 && classSelectSubjectCount > 0 {
-				classAttendanceResponseItem.AttendancePercentage = float64(studentSelectSubjectAttendanceCount) / float64(studentSelectSubjectCount)
-				classAttendanceResponseItem.ClassAverageAttendancePercentage = float64(classSelectSubjectAttendanceCount) / float64(classSelectSubjectCount)
-			}
-		} else {
-			if studentSelectSubjectCount > 0 && studentUnSelectSubjectCount > 0 && classSelectSubjectCount > 0 {
-				classAttendanceResponseItem.AttendancePercentage = float64(studentSelectSubjectAttendanceCount) / float64(studentSelectSubjectCount)
-				classAttendanceResponseItem.UnSelectedSubjectsAverageAttendancePercentage = float64(studentUnSelectSubjectAttendanceCount) / float64(studentUnSelectSubjectCount)
-				classAttendanceResponseItem.ClassAverageAttendancePercentage = float64(classSelectSubjectAttendanceCount) / float64(classSelectSubjectCount)
-			}
+		// when select subject all , calculate only attendancePercentage,classAverageAttendancePercentage
+		if len(request.UnSelectedSubjectIDList) == 0 && studentSelectSubjectCount > 0 && classSelectSubjectCount > 0 {
+			classAttendanceResponseItem.AttendancePercentage = float64(studentSelectSubjectAttendanceCount) / float64(studentSelectSubjectCount)
+			classAttendanceResponseItem.ClassAverageAttendancePercentage = float64(classSelectSubjectAttendanceCount) / float64(classSelectSubjectCount)
+		} else if studentSelectSubjectCount > 0 && studentUnSelectSubjectCount > 0 && classSelectSubjectCount > 0 {
+			classAttendanceResponseItem.AttendancePercentage = float64(studentSelectSubjectAttendanceCount) / float64(studentSelectSubjectCount)
+			classAttendanceResponseItem.UnSelectedSubjectsAverageAttendancePercentage = float64(studentUnSelectSubjectAttendanceCount) / float64(studentUnSelectSubjectCount)
+			classAttendanceResponseItem.ClassAverageAttendancePercentage = float64(classSelectSubjectAttendanceCount) / float64(classSelectSubjectCount)
 		}
-		//reset
-		classSelectSubjectCount = 0
-		classSelectSubjectAttendanceCount = 0
-		studentSelectSubjectCount = 0
-		studentSelectSubjectAttendanceCount = 0
-		studentUnSelectSubjectCount = 0
-		studentUnSelectSubjectAttendanceCount = 0
 	}
 	return
-}
-
-func isExistInArray(value string, array []string) bool {
-	for _, v := range array {
-		if v == value {
-			return true
-		}
-	}
-	return false
 }
