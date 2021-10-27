c45186a3ac086796bb9a90bc126f8ef2e6cef832
allen <allen.shen@badamuna.com.cn>
2021-10-27 19:11:27 +0800

    fix(nkl-1349): fix report class attendance bug

--- da/report_class_attendance.go
+++ da/report_class_attendance.go
@@ -14,36 +14,34 @@ type IClassAttendance interface {
 
 func (r *ReportDA) GetClassAttendance(ctx context.Context, request *entity.ClassAttendanceQueryParameters) (model []*entity.ClassAttendance, err error) {
 	sql := `
-select s1.class_id,s1.subject_id,s2.student_id,if(s2.finish_counts > 0,true,false) as is_attendance
-from 
-    (
-     select sr.schedule_id,
-            sr.relation_id                                as class_id,
-            if(sr1.relation_id is null, '', sr1.relation_id) as subject_id,
-            sh.start_at,sh.class_type
-            from   schedules_relations sr
-            left join schedules_relations sr1 
-            on sr.schedule_id = sr1.schedule_id
-            inner join schedules sh
-            on sr.schedule_id = sh.id
-            where sr.relation_type = '{{.ClassRosterClass}}' and sr1.relation_type = '{{.Subject}}'
-    )
-as s1
-inner join
-   (
-    select sr.relation_id as student_id,car.class_id,car.schedule_id,if(car.finish_counts is null, 0, car.finish_counts) as finish_counts
-    from schedules_relations sr 
-    left join classes_assignments_records  car
-    on sr.schedule_id=car.schedule_id
-    and sr.relation_id=car.attendance_id
-    where sr.relation_type='{{.ClassRosterStudent}}'
-   )
-as s2
-on s1.schedule_id=s2.schedule_id
-and s1.class_id=s2.class_id
-and s1.class_type='{{.OnlineClass}}'
-where  s1.class_id=? and s1.start_at>=? and s1.start_at<? and s1.subject_id in (?)
-order by s1.class_id,s1.subject_id
+select s3.student_id,s3.class_id,s3.subject_id,if(car.finish_counts > 0,true,false) as is_attendance from 
+(
+	select s1.student_id,s1.schedule_id,s2.class_id,s2.subject_id,s2.start_at,s2.Title,s2.class_type from 
+	(
+   	select sr.relation_id as student_id,sr.schedule_id from schedules_relations sr
+   	where sr.relation_type='{{.ClassRosterStudent}}' 
+	) s1 left join 
+	(
+		select sr.schedule_id,
+		sr.relation_id as class_id,
+		if(sr1.relation_id is null, '', sr1.relation_id) as subject_id,
+		sh.start_at,sh.class_type,sh.Title
+		from schedules_relations sr
+		left join schedules_relations sr1
+		on sr.schedule_id = sr1.schedule_id
+		inner join schedules sh
+		on sr.schedule_id = sh.id
+		where sr.relation_type = '{{.ClassRosterClass}}' and sr1.relation_type = '{{.Subject}}' 
+	) s2
+	on s1.schedule_id=s2.schedule_id 
+) s3 left join classes_assignments_records car
+on s3.schedule_id=car.schedule_id
+and s3.student_id=car.attendance_id
+and s3.class_id=car.class_id
+where s3.class_type='{{.OnlineClass}}' and s3.class_id=?
+and s3.start_at>=? and s3.start_at<?
+and s3.subject_id in (?)
+order by s3.class_id,s3.subject_id
 `
 	sqlTmplParameter := struct {
 		ClassRosterClass   string
