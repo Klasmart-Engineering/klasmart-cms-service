7ba3aac85f88c6eee94ef951eee713fb44f8f22f
Kyle Ye <kyle.ye@kidsloop.cn>
2021-10-27 16:54:07 +0800

    fix(NKL-1378): refactor outcome relation da structure

--- entity/relation.go
+++ entity/relation.go
@@ -37,7 +37,13 @@ func (MilestoneRelation) TableName() string {
 }
 
 type OutcomeRelation struct {
-	Relation
+	ID           int64        `gorm:"column:id;primary_key"`
+	MasterID     string       `gorm:"column:master_id"`
+	MasterType   RelationType `gorm:"column:master_type"`
+	RelationID   string       `gorm:"column:relation_id"`
+	RelationType RelationType `gorm:"column:relation_type"`
+	CreateAt     int64        `gorm:"column:create_at" json:"created_at"`
+	UpdateAt     int64        `gorm:"column:update_at" json:"updated_at"`
 }
 
 func (OutcomeRelation) TableName() string {
