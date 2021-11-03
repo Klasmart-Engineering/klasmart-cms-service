bc1e920a8d35334606680f6b05d312d0295af88a
peter.yang <peter.yang@badanamu.com.cn>
2021-11-03 17:32:31 +0800

    refactor(NKL-1414): remove useless function in content da

--- da/content.go
+++ da/content.go
@@ -3,10 +3,8 @@ package da
 import (
 	"context"
 	"fmt"
-	"regexp"
 	"strings"
 	"sync"
-	"unicode"
 
 	"gitlab.badanamu.com.cn/calmisland/common-log/log"
 	"gitlab.badanamu.com.cn/calmisland/dbo"
@@ -241,15 +239,6 @@ func (s *ContentCondition) GetPager() *dbo.Pager {
 }
 func (s *ContentCondition) GetOrderBy() string {
 	return s.OrderBy.ToSQL()
-}
-
-func IsChineseChar(str string) bool {
-	for _, r := range str {
-		if unicode.Is(unicode.Scripts["Han"], r) || (regexp.MustCompile("[\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b]").MatchString(string(r))) {
-			return true
-		}
-	}
-	return false
 }
 
 // NewTeacherOrderBy parse order by
