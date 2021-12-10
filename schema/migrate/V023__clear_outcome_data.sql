7a6c9c4b6d3ba66a9a4c627f2a6033cb868277be
Kyle Ye <kyle.ye@kidsloop.cn>
2021-12-10 16:29:39 +0800

    fix(KLA-331): clean outcome relation sql script

--- schema/migrate/V023__clear_outcome_data.sql
+++ schema/migrate/V023__clear_outcome_data.sql
@@ -1,3 +1,180 @@
+-- Migrate program of learning outcome
+INSERT INTO outcomes_relations ( master_id, relation_id, relation_type, master_type, create_at, update_at, delete_at ) SELECT
+learning_outcomes.id AS master_id,
+SUBSTRING_INDEX( SUBSTRING_INDEX( learning_outcomes.program, ',', numbers.n ), ',', - 1 ) AS relation_id,
+"program" AS relation_type,
+"outcome" AS master_type,
+learning_outcomes.create_at AS create_at,
+learning_outcomes.update_at AS update_at,
+IFNULL( learning_outcomes.delete_at, 0 ) AS delete_at 
+FROM
+	(
+	SELECT
+		1 AS n UNION ALL
+	SELECT
+		2 UNION ALL
+	SELECT
+		3 UNION ALL
+	SELECT
+		4 UNION ALL
+	SELECT
+		5 UNION ALL
+	SELECT
+		6 UNION ALL
+	SELECT
+		7 UNION ALL
+	SELECT
+		8 UNION ALL
+	SELECT
+		9 UNION ALL
+	SELECT
+		10 UNION ALL
+	SELECT
+		11 UNION ALL
+	SELECT
+		12 UNION ALL
+	SELECT
+		13 UNION ALL
+	SELECT
+		14 UNION ALL
+	SELECT
+		15 UNION ALL
+	SELECT
+		16 UNION ALL
+	SELECT
+		17 UNION ALL
+	SELECT
+		18 UNION ALL
+	SELECT
+		19 UNION ALL
+	SELECT
+		20 
+	) numbers
+	INNER JOIN learning_outcomes ON LENGTH( learning_outcomes.program ) > 0 
+	AND LENGTH( learning_outcomes.program ) - LENGTH(
+	REPLACE ( learning_outcomes.program, ',', '' ))>= numbers.n - 1 
+GROUP BY
+	id,
+	relation_id;
+
+-- Migrate subject of learning outcome
+INSERT INTO outcomes_relations ( master_id, relation_id, relation_type, master_type, create_at, update_at, delete_at ) SELECT
+learning_outcomes.id AS master_id,
+SUBSTRING_INDEX( SUBSTRING_INDEX( learning_outcomes.subject, ',', numbers.n ), ',', - 1 ) AS relation_id,
+"subject" AS relation_type,
+"outcome" AS master_type,
+learning_outcomes.create_at AS create_at,
+learning_outcomes.update_at AS update_at,
+IFNULL( learning_outcomes.delete_at, 0 ) AS delete_at 
+FROM
+	(
+	SELECT
+		1 AS n UNION ALL
+	SELECT
+		2 UNION ALL
+	SELECT
+		3 UNION ALL
+	SELECT
+		4 UNION ALL
+	SELECT
+		5 UNION ALL
+	SELECT
+		6 UNION ALL
+	SELECT
+		7 UNION ALL
+	SELECT
+		8 UNION ALL
+	SELECT
+		9 UNION ALL
+	SELECT
+		10 UNION ALL
+	SELECT
+		11 UNION ALL
+	SELECT
+		12 UNION ALL
+	SELECT
+		13 UNION ALL
+	SELECT
+		14 UNION ALL
+	SELECT
+		15 UNION ALL
+	SELECT
+		16 UNION ALL
+	SELECT
+		17 UNION ALL
+	SELECT
+		18 UNION ALL
+	SELECT
+		19 UNION ALL
+	SELECT
+		20 
+	) numbers
+	INNER JOIN learning_outcomes ON LENGTH( learning_outcomes.subject ) > 0 
+	AND LENGTH( learning_outcomes.subject ) - LENGTH(
+	REPLACE ( learning_outcomes.subject, ',', '' ))>= numbers.n - 1 
+GROUP BY
+	id,
+	relation_id;
+
+-- Migrate category of learning outcome
+INSERT INTO outcomes_relations ( master_id, relation_id, relation_type, master_type, create_at, update_at, delete_at ) SELECT
+learning_outcomes.id AS master_id,
+SUBSTRING_INDEX( SUBSTRING_INDEX( learning_outcomes.developmental, ',', numbers.n ), ',', - 1 ) AS relation_id,
+"category" AS relation_type,
+"outcome" AS master_type,
+learning_outcomes.create_at AS create_at,
+learning_outcomes.update_at AS update_at,
+IFNULL( learning_outcomes.delete_at, 0 ) AS delete_at 
+FROM
+	(
+	SELECT
+		1 AS n UNION ALL
+	SELECT
+		2 UNION ALL
+	SELECT
+		3 UNION ALL
+	SELECT
+		4 UNION ALL
+	SELECT
+		5 UNION ALL
+	SELECT
+		6 UNION ALL
+	SELECT
+		7 UNION ALL
+	SELECT
+		8 UNION ALL
+	SELECT
+		9 UNION ALL
+	SELECT
+		10 UNION ALL
+	SELECT
+		11 UNION ALL
+	SELECT
+		12 UNION ALL
+	SELECT
+		13 UNION ALL
+	SELECT
+		14 UNION ALL
+	SELECT
+		15 UNION ALL
+	SELECT
+		16 UNION ALL
+	SELECT
+		17 UNION ALL
+	SELECT
+		18 UNION ALL
+	SELECT
+		19 UNION ALL
+	SELECT
+		20 
+	) numbers
+	INNER JOIN learning_outcomes ON LENGTH( learning_outcomes.developmental ) > 0 
+	AND LENGTH( learning_outcomes.developmental ) - LENGTH(
+	REPLACE ( learning_outcomes.developmental, ',', '' ))>= numbers.n - 1 
+GROUP BY
+	id,
+	relation_id;
+
 -- Migrate subcategory of learning outcome
 INSERT INTO outcomes_relations ( master_id, relation_id, relation_type, master_type, create_at, update_at, delete_at ) SELECT
 learning_outcomes.id AS master_id,
