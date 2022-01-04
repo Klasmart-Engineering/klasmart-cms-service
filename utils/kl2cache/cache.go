2bf31f37adc23f88c3a3833c0438ae970cca9f87
Chuanbing <chuanbing.lu@badanamu.com.cn>
2022-01-04 18:01:43 +0800

    feat(CMS-242): bugfix for kl2cache disabled

--- utils/kl2cache/cache.go
+++ utils/kl2cache/cache.go
@@ -7,7 +7,20 @@ import (
 
 type Provider interface {
 	WithExpireStrategy(ctx context.Context, strategy ExpireStrategy) (provider Provider)
+	// Get set the value of key into val
+	//
+	// fetch data by fGetData if missed
+	//
+	// note: input val of Get must has the same type of output val of fGetDate
+	// val must be a pointer
 	Get(ctx context.Context, key Key, val interface{}, fGetData func(ctx context.Context, key Key) (val interface{}, err error)) (err error)
+	// BatchGet set the values of keys into val
+	//
+	// get values of keys from cache
+	// for missed keys call fGetData to get values, and then set it into cache
+	// set all values(cached and fetched) into val
+	//
+	// note: val must be a pointer of slice, and it's item must has the same type of KeyValue.Val
 	BatchGet(ctx context.Context, keys []Key, val interface{}, fGetData func(ctx context.Context, keys []Key) (kvs []*KeyValue, err error)) (err error)
 }
 
