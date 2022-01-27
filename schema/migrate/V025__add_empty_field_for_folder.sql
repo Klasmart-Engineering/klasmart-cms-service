a016eb7f5ecc89ddbdae62e8eaf56fc9a1cc6e54
blt <brilliant.yang@kidsloop.cn>
2022-01-27 14:23:10 +0800

    feat(CMS-269): schema

--- schema/migrate/V025__add_empty_field_for_folder.sql
+++ schema/migrate/V025__add_empty_field_for_folder.sql
@@ -1,6 +1,6 @@
 alter table cms_folder_items add has_descendant tinyint(1) NOT NULL DEFAULT 0 COMMENT 'has published descendant';
 
-create temporary table has_descendant_folder as (
+create table has_descendant_folder as (
     select distinct substring_index(substring_index(dir_path, '/', b.help_topic_id+1), '/', -1) folder_id
     from cms_contents a join mysql.help_topic b on b.help_topic_id < (length(a.dir_path)-length(replace(a.dir_path, '/', ''))+1)
     where a.publish_status='published' and a.delete_at=0 and a.dir_path<>'/');
