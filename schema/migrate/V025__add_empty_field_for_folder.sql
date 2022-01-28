34167da2a330775f4474a87a0c1065a806f1bff6
blt <brilliant.yang@kidsloop.cn>
2022-01-28 14:22:03 +0800

    feat(CMS-269): database modify

--- schema/migrate/V025__add_empty_field_for_folder.sql
+++ schema/migrate/V025__add_empty_field_for_folder.sql
@@ -1,8 +1,12 @@
 alter table cms_folder_items add has_descendant tinyint(1) NOT NULL DEFAULT 0 COMMENT 'has published descendant';
 
 create table has_descendant_folder as (
-    select distinct substring_index(substring_index(dir_path, '/', b.help_topic_id+1), '/', -1) folder_id
-    from cms_contents a join mysql.help_topic b on b.help_topic_id < (length(a.dir_path)-length(replace(a.dir_path, '/', ''))+1)
+    select distinct substring_index(substring_index(dir_path, '/', b.id+1), '/', -1) folder_id
+    from cms_contents a join (select 1 as id union select 2 as id union select 3 as id union select 4 as id union select 5 as id
+                              union
+                              select 6 as id union select 7 as id union select 8 as id union select 9 as id union select 10 as id
+                              union
+                              select 11 as id union select 12 as id union select 13 as id union select 14 as id union select 15 as id) b on b.id < (length(a.dir_path)-length(replace(a.dir_path, '/', ''))+1)
     where a.publish_status='published' and a.delete_at=0 and a.dir_path<>'/');
 
 update cms_folder_items a, has_descendant_folder b  set has_descendant=1 where a.id=b.folder_id and a.delete_at=0;
