98e3bc49f2be93dfb1650b7ff68d308548833808
kwok <17621739943@163.com>
2022-02-10 16:44:52 +0800

    feat(KLA-320): update migrate assessment data sql

--- schema/migrate/V027__migrate_assessment_data.sql
+++ schema/migrate/V027__migrate_assessment_data.sql
@@ -114,9 +114,19 @@ CREATE TABLE tmp_cms_contents_outcomes(
 
 INSERT IGNORE INTO  tmp_cms_contents_outcomes(content_id,outcome_id)
 SELECT cms_contents.id,
-       substring_index(substring_index(cms_contents.outcomes, ',', b.help_topic_id + 1 ), ',',- 1) outcome_id
+       substring_index(substring_index(cms_contents.outcomes, ',', b.id + 1 ), ',',- 1) outcome_id
 FROM cms_contents
-         JOIN mysql.help_topic b ON b.help_topic_id < (length( cms_contents.outcomes )-length( REPLACE (cms_contents.outcomes, ',', '')) + 1)
+         JOIN
+     (select 0 as id union all select 1 union all select 2 union all select 3 union all select 4 union all select 5
+      union all
+      select 6 union all select 7 union all select 8 union all select 9 union all select 10
+      union all
+      select 11 union all select 12 union all select 13 union all select 14 union all select 15
+      union all
+      select 16 union all select 17 union all select 18 union all select 19 union all select 20
+      union all
+      select 21 union all select 22 union all select 23 union all select 24 union all select 25) b
+     ON b.id < (length( cms_contents.outcomes )-length( REPLACE (cms_contents.outcomes, ',', '')) + 1)
 WHERE cms_contents.outcomes !='' AND cms_contents.outcomes IS NOT NULL;
 
 INSERT IGNORE INTO
