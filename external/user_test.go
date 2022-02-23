a4b810efc9777025a4761b572d4d109b578c06ed
Chuanbing <chuanbing.lu@badanamu.com.cn>
2022-02-23 10:55:21 +0800

    fix(KLR-192): update user_test : use ams_test to init testOperator

--- external/user_test.go
+++ external/user_test.go
@@ -5,8 +5,6 @@ import (
 	"reflect"
 	"testing"
 
-	"gitlab.badanamu.com.cn/calmisland/kidsloop2/config"
-
 	"gitlab.badanamu.com.cn/calmisland/kidsloop2/entity"
 )
 
@@ -27,10 +25,7 @@ func TestAmsUserService_FilterByPermission(t *testing.T) {
 
 func TestAmsUserService_GetUserCount(t *testing.T) {
 	ctx := context.Background()
-	config.Get().AMS.EndPoint = "https://api.alpha.kidsloop.net/user//user"
-	GetUserServiceProvider().GetUserCount(ctx, &entity.Operator{
-		Token: "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFmZGZjMGQ5LWFkYTktNGU2Ni1iMjI1LTIwZjk1NmQxYTM5OSIsImVtYWlsIjoib3JnMTExOUB5b3BtYWlsLmNvbSIsImV4cCI6MTY0NTU4NDU1NCwiaXNzIjoia2lkc2xvb3AifQ.i1Fl8RGmR5D_VLfK9x9A2DUmY1hO1nNUaw8-uxMTeRh_aZJMyHRJfGb33GnyLuM3V54MJVwj-FUBmgMagQz4ePiIHGS0vVbuhkD_ua2S-UhUG31MZVdIS1dywQ_olZG7emFMmTVoPobvhny6Wegi4Srhz861O5da7togly8BftyAjEAnD7LViosm8pIT1p5gqgC6X57VS19I8rDe6D03J6luT4aCOO-Hm5aRZH4ctwIiKaNWBb8YpQhsaU0kxu3X8B3pM88T1q9Ex1P3LlkSauuIYaiu7t2DRLleFwQDTDVflx9e3r92AuV7IhdlogQRJ2vL0eO3Am8yot8Bf0Di4g",
-	}, entity.GetUserCountCondition{
+	GetUserServiceProvider().GetUserCount(ctx, testOperator, entity.GetUserCountCondition{
 		OrgID: entity.NullString{
 			String: "60c064cc-bbd8-4724-b3f6-b886dce4774f",
 			Valid:  true,
