382650aafe2d6a9ac81a8b03eaf6c3dec29ae081
blt <brilliant.yang@kidsloop.cn>
2022-02-28 10:51:53 +0800

    feat(CMS-315): teaching students

--- external/report_teacher_load.go
+++ external/report_teacher_load.go
@@ -38,6 +38,13 @@ type UserFilter struct {
 	} `json:"userId"`
 }
 
+type ClassFilter struct {
+	TeacherID struct {
+		Operator OperatorType `json:"operator"`
+		Value    string       `json:"value"`
+	} `json:"teacherId"`
+}
+
 type UsersConnection struct {
 	Edges []struct {
 		Node struct {
@@ -65,57 +72,100 @@ type UsersConnection struct {
 	} `json:"edges"`
 }
 
+type ClassesConnection struct {
+	Edges []struct {
+		Node struct {
+			ID                 string   `json:"id"`
+			Name               string   `json:"name"`
+			Status             APStatus `json:"status"`
+			StudentsConnection struct {
+				Edges []struct {
+					Node struct {
+						ID         string   `json:"id"`
+						GivenName  string   `json:"givenName"`
+						FamilyName string   `json:"familyName"`
+						Status     APStatus `json:"status"`
+					} `json:"node"`
+				} `json:"edges"`
+			} `json:"studentsConnection"`
+			TeachersConnections struct {
+				Edges []struct {
+					Node struct {
+						ID         string   `json:"id"`
+						GivenName  string   `json:"givenName"`
+						FamilyName string   `json:"familyName"`
+						Status     APStatus `json:"status"`
+					}
+				}
+			} `json:"teachersConnection"`
+		} `json:"node"`
+	} `json:"edges"`
+}
+
 func (t *AmsTeacherLoadService) BatchGetActiveClassWithStudent(ctx context.Context, operator *entity.Operator, teacherIDs []string) (map[string]*TeacherClassWithStudent, error) {
 
 	ids := utils.SliceDeduplicationExcludeEmpty(teacherIDs)
 	if len(ids) == 0 {
 		return map[string]*TeacherClassWithStudent{}, nil
 	}
-	orFilters := make([]UserFilter, len(ids))
-	for i, v := range ids {
+	classOr := make([]ClassFilter, 0, len(ids))
+	userOr := make([]UserFilter, 0, len(ids))
+	for _, v := range ids {
+		cf := ClassFilter{}
+		cf.TeacherID.Operator = OperatorTypeEq
+		cf.TeacherID.Value = v
+		classOr = append(classOr, cf)
+
 		uf := UserFilter{}
 		uf.UserID.Operator = OperatorTypeEq
 		uf.UserID.Value = v
-		orFilters[i] = uf
+		userOr = append(userOr, uf)
 	}
 
 	query := `
-query ($or:[UserFilter!]){
-	usersConnection(direction:BACKWARD, filter:{OR:$or}) {
-		edges{
-		  node{
-			id
-			classesTeachingConnection(filter:{status: {operator: eq, value: "active"}}){
-			  edges{
-				node{
-				  id
-				  name
-				  status
-				  studentsConnection(filter:{userStatus:{operator: eq, value: "active"}, organizationUserStatus:{operator:eq, value: "active"}}){
-					edges{
-					  node{
-						id
-						givenName
-						familyName
-						status
-					  }
-					}
-				  }
-				}
-			  }
-			}
-		  }
-		}
-	} 
-}`
+query ($classOr:[ClassFilter!], $userOr:[UserFilter!]){
+	classesConnection(direction:BACKWARD, filter:{status: {operator: eq, value: "active"}, OR: $classOr}) {
+    totalCount
+    edges{
+      node{
+        id
+        name
+        status
+        studentsConnection(direction:BACKWARD, filter:{userStatus: {operator:eq, value:"active"}}){
+ 					totalCount
+          edges{
+            node{
+              id
+              givenName
+              familyName
+              status
+            }
+          }
+        }
+        teachersConnection(direction:BACKWARD, filter:{OR: $userOr}){
+          edges{
+            node{
+              id
+              givenName
+              familyName
+              status
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
 
 	request := chlorine.NewRequest(query, chlorine.ReqToken(operator.Token))
-	request.Var("or", orFilters)
+	request.Var("classOr", classOr)
+	request.Var("userOr", userOr)
 
-	var data UsersConnection
+	var data ClassesConnection
 	response := &chlorine.Response{
 		Data: &struct {
-			*UsersConnection `json:"usersConnection"`
+			*ClassesConnection `json:"classesConnection"`
 		}{
 			&data,
 		},
@@ -135,20 +185,24 @@ query ($or:[UserFilter!]){
 		return nil, err
 	}
 	result := make(map[string]*TeacherClassWithStudent, len(data.Edges))
-	for _, teacher := range data.Edges {
-		tcs := TeacherClassWithStudent{}
-		tcs.UserID = teacher.Node.ID
-		tcs.ClassesTeaching = make([]*ClassStudents, len(teacher.Node.ClassesTeachingConnection.Edges))
-		for i, class := range teacher.Node.ClassesTeachingConnection.Edges {
-			classStudents := ClassStudents{ClassID: class.Node.ID}
-			classStudents.Students = make([]*StudentInClass, len(class.Node.StudentsConnection.Edges))
-			for j, student := range class.Node.StudentsConnection.Edges {
-				classStudents.Students[j] = &StudentInClass{student.Node.ID}
-			}
-			tcs.ClassesTeaching[i] = &classStudents
+
+	for _, class := range data.Edges {
+		classStudents := ClassStudents{ClassID: class.Node.ID}
+		classStudents.Students = make([]*StudentInClass, 0, len(class.Node.StudentsConnection.Edges))
+		for _, student := range class.Node.StudentsConnection.Edges {
+			classStudents.Students = append(classStudents.Students, &StudentInClass{UserID: student.Node.ID})
 		}
-		result[tcs.UserID] = &tcs
+
+		for _, teacher := range class.Node.TeachersConnections.Edges {
+			if _, ok := result[teacher.Node.ID]; !ok {
+				result[teacher.Node.ID] = &TeacherClassWithStudent{
+					UserID: teacher.Node.ID,
+				}
+			}
+			result[teacher.Node.ID].ClassesTeaching = append(result[teacher.Node.ID].ClassesTeaching, &classStudents)
+		}
 	}
+
 	return result, err
 }
 
