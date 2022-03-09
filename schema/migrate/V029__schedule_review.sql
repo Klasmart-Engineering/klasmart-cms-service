7314f90d506cabc6897c5ba00d99dcd15572da11
Kyle Ye <kyle.ye@kidsloop.cn>
2022-03-09 12:29:34 +0800

    feat(KLS-318): update flyway script

--- schema/migrate/V029__schedule_review.sql
+++ schema/migrate/V029__schedule_review.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `schedules_reviews` (
   `schedule_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'schedule_id',
   `student_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'student_id',
-  `review_status` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'review_status'
+  `review_status` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'review_status',
+  `live_lesson_plan` json COMMENT 'review lesson plan'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='schedules_reviews';
 
 ALTER TABLE schedules ADD is_review tinyint(1) NOT NULL DEFAULT '0' COMMENT 'is_review';
