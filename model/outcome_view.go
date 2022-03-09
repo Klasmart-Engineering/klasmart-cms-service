86394698a341f00135c61b5acfe489015e4fadc1
Kwok <17621739943@163.com>
2022-03-09 12:44:19 +0800

    feat(KLA-376): add score_threshold field to learning_outcomes

--- model/outcome_view.go
+++ model/outcome_view.go
@@ -3,6 +3,7 @@ package model
 import (
 	"context"
 	"errors"
+	"gitlab.badanamu.com.cn/calmisland/kidsloop2/constant"
 	"regexp"
 
 	"strings"
@@ -41,6 +42,7 @@ type OutcomeCreateView struct {
 	Description    string                  `json:"description"`
 	Shortcode      string                  `json:"shortcode,omitempty"`
 	Sets           []*OutcomeSetCreateView `json:"sets"`
+	ScoreThreshold float32                 `json:"score_threshold"`
 }
 
 type OutcomeSetCreateView struct {
@@ -106,6 +108,13 @@ func (req OutcomeCreateView) ToOutcome(ctx context.Context, op *entity.Operator)
 		outcome.Sets[i] = set
 	}
 
+	if !req.Assumed && req.ScoreThreshold <= 0 {
+		log.Warn(ctx, "score threshold need set value when assumed is false", log.Any("req", req))
+		return nil, constant.ErrInvalidArgs
+	} else {
+		req.ScoreThreshold = 0
+	}
+
 	return &outcome, nil
 }
 
@@ -118,6 +127,14 @@ func (req OutcomeCreateView) ToOutcomeWithID(ctx context.Context, op *entity.Ope
 		return nil, errors.New("outcomeID invalid")
 	}
 	outcome.ID = outcomeID
+
+	if !req.Assumed && req.ScoreThreshold <= 0 {
+		log.Warn(ctx, "score threshold need set value when assumed is false", log.Any("req", req))
+		return nil, constant.ErrInvalidArgs
+	} else {
+		req.ScoreThreshold = 0
+	}
+
 	return outcome, nil
 }
 
