59bb476ad59a59d37ad9ec3d098ecb0612f7cc41
Kyle Ye <kyle.ye@kidsloop.cn>
2022-03-16 11:05:48 +0800

    feat(KLS-391): merge flyway script

--- schema/migrate/V029__schedule_review.sql
+++ schema/migrate/V029__schedule_review.sql
@@ -9,3 +9,6 @@ ALTER TABLE schedules ADD is_review tinyint(1) NOT NULL DEFAULT '0' COMMENT 'is_
 ALTER TABLE schedules ADD review_status varchar(100) NOT NULL DEFAULT '' COMMENT 'review_status';
 ALTER TABLE schedules ADD content_start_at bigint(20) NOT NULL DEFAULT '0' COMMENT 'content_start_at';
 ALTER TABLE schedules ADD content_end_at bigint(20) NOT NULL DEFAULT '0' COMMENT 'content_end_at';
+
+ALTER TABLE schedules_reviews ADD `type` varchar(100) NOT NULL DEFAULT '' COMMENT 'review_type';
+ALTER TABLE schedules_reviews ADD UNIQUE INDEX uk_schedule_id_student_id ( schedule_id, student_id );
