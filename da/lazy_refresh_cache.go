2276c6352f73fece03ed2f0ed024c5af92cc84ad
peter.yang <peter.yang@badanamu.com.cn>
2022-03-23 16:41:28 +0800

    feat(CMS-331): use new context in async goroutine, to avoid context cancel

--- da/lazy_refresh_cache.go
+++ da/lazy_refresh_cache.go
@@ -49,6 +49,10 @@ func (c LazyRefreshCache) Get(ctx context.Context, request, response interface{}
 	data := &lazyRefreshCacheData{Data: response}
 	err := c.cache.Param(hash).GetObject(ctx, data)
 	if err == redis.Nil {
+		log.Debug(ctx, "lazy refresh cache miss",
+			log.String("hash", hash),
+			log.Any("requst", request))
+
 		// query cache for the first time, let's refresh it
 		err = c.refreshCache(ctx, hash, request)
 		if err != nil {
@@ -61,13 +65,14 @@ func (c LazyRefreshCache) Get(ctx context.Context, request, response interface{}
 		return err
 	}
 
-	log.Debug(ctx, "get cached data successfully",
+	log.Debug(ctx, "lazy refresh cache hit",
 		log.String("hash", hash),
 		log.Any("requst", request),
 		log.Any("data", data))
 
 	// cache refresh trigger
 	go func() {
+		ctx = utils.CloneContextWithTrace(ctx)
 		err := c.checkVersion(ctx, hash, request, data.Version)
 		if err != nil {
 			log.Warn(ctx, "check version failed", log.Err(err), log.String("hash", hash))
