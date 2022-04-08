74e6d7819a89201481ec66882d84db2f3b558c1f
Kwok <17621739943@163.com>
2022-04-08 14:50:14 +0800

    feat(KLA-404): update migrate hfs assessment data sql

--- schema/migrate/V031__migrate_hfs_assessment_data.sql
+++ schema/migrate/V031__migrate_hfs_assessment_data.sql
@@ -2,8 +2,11 @@
 alter table feedbacks_assignments add review_attachment_id varchar(500) DEFAULT NULL COMMENT 'attachment id for teacher evaluation';
 
 -- migrate hsf assessment data
-select
-assessments_v2.id
+update assessments_v2,
+(select
+assessments_v2.id id,
+if(sum(if(assessments_reviewer_feedback_v2.status='Complete',0,1))=0,'Complete',
+if(sum(if(assessments_reviewer_feedback_v2.status is null,0,1))=0,'NotStarted','Started')) status
 from
 assessments_v2
 left join
@@ -16,5 +19,6 @@ where
 assessments_v2.assessment_type = "OfflineStudy"
 and assessments_v2.delete_at=0
 and assessments_users_v2.user_type='Student'
-group by assessments_v2.id
-having sum(if(assessments_reviewer_feedback_v2.status='Complete',0,1))=0;
+group by assessments_v2.id) t2
+set assessments_v2.status = t2.status
+where assessments_v2.id=t2.id;
