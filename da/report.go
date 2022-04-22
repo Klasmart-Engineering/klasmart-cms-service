6f09e9cb109935e844ede60ff6970cb8a902a4e3
peter.yang <peter.yang@badanamu.com.cn>
2022-04-22 17:14:47 +0800

    feat(KLR-229): add query cache for teacher usage overview

--- da/report.go
+++ da/report.go
@@ -22,6 +22,7 @@ type ReportDA struct {
 	BaseDA
 	learnerReportOverviewCache   *LazyRefreshCache
 	learningOutcomeOverviewCache *LazyRefreshCache
+	teacherUsageOverviewCache    *LazyRefreshCache
 }
 
 var _reportDA *ReportDA
@@ -52,6 +53,17 @@ func GetReportDA() IReportDA {
 		}
 
 		_reportDA.learningOutcomeOverviewCache = learningOutcomeOverviewCache
+
+		teacherUsageOverviewCache, err := NewLazyRefreshCache(&LazyRefreshCacheOption{
+			RedisKeyPrefix:  RedisKeyPrefixReportTeacherUsageOverview,
+			Expiration:      constant.ReportQueryCacheExpiration,
+			RefreshDuration: constant.ReportQueryCacheRefreshDuration,
+			RawQuery:        _reportDA.getTeacherUsageOverview})
+		if err != nil {
+			log.Panic(context.Background(), "create teacher usage overview cache failed", log.Err(err))
+		}
+
+		_reportDA.teacherUsageOverviewCache = teacherUsageOverviewCache
 
 	})
 	return _reportDA
